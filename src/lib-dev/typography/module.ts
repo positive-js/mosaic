29fe0c003d4a8e60d10a3d3337e8db3655fb649f
Oleg Pimenov <opimenov@ptsecurity.com>
2017-06-21 23:01:16 +0300

    fixed typography

--- src/lib-dev/typography/module.ts
+++ src/lib-dev/typography/module.ts
@@ -1,3 +1,50 @@
-/* tslint:disable:no-var-requires */
-require('./main.scss');
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootloader } from '../helpers';
+
+
+@Component({
+    selector: 'app',
+    template: `
+        <div class="demo-typography">
+            <span class="mc-display-4">Display 4</span>
+            <span class="mc-display-3">Display 3</span>
+            <span class="mc-display-2">Display 2</span>
+            <span class="mc-display-1">Display 1</span>
+
+            <span class="mc-headline">Headline</span>
+            <span class="mc-title">Title</span>
+            <span class="mc-subheading">Subheading</span>
+
+            <span class="mc-body-2">Body 2</span>
+            <span class="mc-body-1">Body 1</span>
+        </div>`,
+    styleUrls: ['./main.scss'],
+})
+export class DemoComponent {}
+
+
+/* tslint:disable:max-classes-per-file */
+@NgModule({
+    declarations: [
+        DemoComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    bootstrap: [
+        DemoComponent
+    ]
+})
+export class DemoModule {}
+
+function main() {
+    return platformBrowserDynamic()
+        .bootstrapModule(DemoModule)
+        /* tslint:disable:no-console */
+        .catch((error) => console.error(error));
+}
+
+bootloader(main);
 
