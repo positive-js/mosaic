cd83dd14d322af734223cff919628567347dc25b
Oleg Pimenov <opimenov@ptsecurity.com>
2018-01-20 20:16:49 +0300

    (webpack) added stats

--- tools/webpack/webpack.lib.dev.js
+++ tools/webpack/webpack.lib.dev.js
@@ -4,7 +4,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
 const DefinePlugin = require('webpack/lib/DefinePlugin');
 const HotModuleReplacementPlugin = require('webpack/lib/HotModuleReplacementPlugin');
-
 const helpers = require('./helpers');
 const commonConfig = require('./webpack.common.js');
 
@@ -75,7 +74,6 @@ module.exports = function (options) {
         },
 
         plugins: [
-
             new CleanWebpackPlugin(
                 ['build'],
                 {
@@ -110,7 +108,19 @@ module.exports = function (options) {
             watchOptions: {
                 ignored: /node_modules/
             },
-            stats: 'minimal'
+            stats: {
+                colors: true,
+                hash: true,
+                timings: true,
+                chunks: true,
+                chunkModules: false,
+                children: false, // listing all children is very noisy in AOT and hides warnings/errors
+                modules: false,
+                reasons: false,
+                warnings: true,
+                assets: false, // listing all assets is very noisy when using assets directories
+                version: false
+            }
         }
     })
 };
