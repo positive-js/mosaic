345d071e7e99850f5caf7bf682f562a5e33f06b1
lkramarov <lkramarov@ptsecurity.com>
2018-03-12 11:09:28 +0300

    warn style for button and hover

--- src/lib-dev/button/module.ts
+++ src/lib-dev/button/module.ts
@@ -8,28 +8,28 @@ import { McButtonModule } from '../../lib/button/';
 @Component({
     selector: 'app',
     template: `
-        <button mc-xs-button>mc-xs-button</button>
-        <button mc-sm-button>mc-sm-button</button>
-        <button mc-button>default</button>
-        <button mc-lg-button>mc-lg-button</button>
-        <button mc-xl-button>mc-xl-button</button>
+        <button mc-xs-button>mc-xs-button</button>&nbsp;
+        <button mc-sm-button>mc-sm-button</button>&nbsp;
+        <button mc-button>default</button>&nbsp;
+        <button mc-lg-button>mc-lg-button</button>&nbsp;
+        <button mc-xl-button>mc-xl-button</button>&nbsp;
         
         <br>
+        <br>
         
-        <a href="" mc-xs-button>mc-xs-button</a>
-        <a href="" mc-sm-button>mc-sm-button</a>
-        <a href="" mc-button>default</a>
-        <a href="" mc-lg-button>mc-lg-button</a>
-        <a href="" mc-xl-button>mc-xl-button</a>
+        <a href="" mc-xs-button>mc-xs-button</a>&nbsp;
+        <a href="" mc-sm-button>mc-sm-button</a>&nbsp;
+        <a href="" mc-button>default</a>&nbsp;
+        <a href="" mc-lg-button>mc-lg-button</a>&nbsp;
+        <a href="" mc-xl-button>mc-xl-button</a>&nbsp;
         
+        <br>
         <br>
 
         <button mc-button color="primary" disabled>disabled</button>&nbsp;
         <button mc-button color="primary">normal</button>&nbsp;
         <button class="mc-hover" mc-button color="primary">hover</button>&nbsp;
         <button class="cdk-focused" mc-button color="primary">focus</button>&nbsp;
-        <button class="mc-active" mc-button color="primary">pressed</button>&nbsp;
-        <button class="mc-active mc-hover" mc-button color="primary">pressed-hover</button>
 
         <br>
         <br>
@@ -38,8 +38,6 @@ import { McButtonModule } from '../../lib/button/';
         <button class="mc-progress" mc-button color="primary">normal</button>&nbsp;
         <button class="mc-hover mc-progress" mc-button color="primary">hover</button>&nbsp;
         <button class="cdk-focused mc-progress" mc-button color="primary">focus</button>&nbsp;
-        <button class="mc-active mc-progress" mc-button color="primary">pressed</button>&nbsp;
-        <button class="mc-active mc-hover mc-progress" mc-button color="primary">pressed-hover</button>
 
         <br>
         <br>
@@ -48,8 +46,6 @@ import { McButtonModule } from '../../lib/button/';
         <button mc-button color="second">normal</button>&nbsp;
         <button class="mc-hover" mc-button color="second">hover</button>&nbsp;
         <button class="cdk-focused" mc-button color="second">focus</button>&nbsp;
-        <button class="mc-active" mc-button color="second">pressed</button>&nbsp;
-        <button class="mc-active mc-hover" mc-button color="second">pressed-hover</button>
         
         <br>
         <br>
@@ -58,12 +54,22 @@ import { McButtonModule } from '../../lib/button/';
         <button class="mc-progress" mc-button color="second">normal</button>&nbsp;
         <button class="mc-hover mc-progress" mc-button color="second">hover</button>&nbsp;
         <button class="cdk-focused mc-progress" mc-button color="second">focus</button>&nbsp;
-        <button class="mc-active mc-progress" mc-button color="second">pressed</button>&nbsp;
-        <button class="mc-active mc-hover mc-progress" mc-button color="second">pressed-hover</button>
         
-        <br><br>
+        <br>
+        <br>
 
-        <button mc-button color="warn">warn</button>
+        <button mc-button color="warn" disabled>disabled</button>&nbsp;
+        <button mc-button color="warn">normal</button>&nbsp;
+        <button class="mc-hover" mc-button color="warn">hover</button>&nbsp;
+        <button class="cdk-focused" mc-button color="warn">focus</button>&nbsp;
+
+        <br>
+        <br>
+
+        <button class="mc-progress" mc-button color="warn" disabled>disabled</button>&nbsp;
+        <button class="mc-progress" mc-button color="warn">normal</button>&nbsp;
+        <button class="mc-hover mc-progress" mc-button color="warn">hover</button>&nbsp;
+        <button class="cdk-focused mc-progress" mc-button color="warn">focus</button>&nbsp;
     `,
     styleUrls: ['./theme.scss']
 })
