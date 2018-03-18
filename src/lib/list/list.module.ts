e03bbd50cd822643cc5f30d8fdcab0614ccb70d8
Oleg Pimenov <pimenov.o@gmail.com>
2018-03-18 16:38:15 +0300

    mv platformModule using in CDK

--- src/lib/list/list.module.ts
+++ src/lib/list/list.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { A11yModule } from '@ptsecurity/cdk/a11y';
-import { PlatformModule } from '@ptsecurity/cdk/platform';
 
 import { McLineModule, McPseudoCheckboxModule } from '@ptsecurity/mosaic/core';
 
@@ -13,7 +12,6 @@ import { McList, McListItem, McListSubheaderCssStyler} from './list.component';
 @NgModule({
     imports: [
         CommonModule,
-        PlatformModule,
         A11yModule,
         McPseudoCheckboxModule,
         McLineModule
