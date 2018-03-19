6817477718a2529a33cc53fba6d72356bbcd1977
Oleg Pimenov <pimenov.o@gmail.com>
2018-03-20 01:12:20 +0300

    docs publish

---
+++ scripts/deploy/publish-docs.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname $0)/../../"
+
+
+$(npm bin)/gulp docs
+
+# Path to the project directory.
+projectPath="$(pwd)"
+
+# Path to the directory that contains the generated docs output.
+docsDistPath="${projectPath}/dist/docs"
+
+# Path to the cloned docs-content repository.
+docsContentPath="${projectPath}/tmp/mosaic-docs-builds"
+
+# Git clone URL for the repository.
+docsContentRepoUrl="https://github.com/positive-js/mosaic-docs-builds"
+
+# Current version of Mosaic from the package.json file
+buildVersion=$(node -pe "require('./package.json').version")
+
+# Name of the branch that is currently being deployed.
+branchName=${CIRCLE_BRANCH:-'master'}
+
+# Additional information about the last commit for docs-content commits.
+commitSha=$(git rev-parse --short HEAD)
+commitAuthorName=$(git --no-pager show -s --format='%an' HEAD)
+commitAuthorEmail=$(git --no-pager show -s --format='%ae' HEAD)
+commitMessage=$(git log --oneline -n 1)
+commitTag="${buildVersion}-${commitSha}"
+
+buildVersionName="${buildVersion}-${commitSha}"
+buildTagName="${branchName}-${commitSha}"
+buildCommitMessage="${branchName} - ${commitMessage}"
+
+echo "Starting deployment of the docs-build for ${buildVersionName} in ${branchName}"
+
+# Remove the docs-content repository if the directory exists
+rm -Rf ${docsContentPath}
+
+# Clone the docs-content repository.
+git clone ${docsContentRepoUrl} ${docsContentPath} --depth 1
+
+echo "Successfully cloned docs-content repository."
+
+# Go into the repository directory.
+cd ${docsContentPath}
+
+echo "Switched into the repository directory."
+
+if [[ $(git ls-remote --heads origin ${branchName}) ]]; then
+  git checkout ${branchName}
+  echo "Switched to ${branchName} branch."
+else
+  echo "Branch ${branchName} does not exist on the docs-content repo yet. Creating ${branchName}.."
+  git checkout -b ${branchName}
+  echo "Branch created and checked out."
+fi
+
+# Remove everything inside of the docs-content repository.
+rm -Rf ${docsContentPath}/*
+
+echo "Removed everything from the docs-content repository. Copying all files into repository.."
+
+# Create all folders that need to exist in the docs-content repository.
+mkdir ${docsContentPath}/{overview,guides,api,examples,stackblitz,examples-package}
+
+# Copy API and example files to the docs-content repository.
+cp -R ${docsDistPath}/api/* ${docsContentPath}/api
+
+
+# Copy the license file to the docs-content repository.
+cp ${projectPath}/LICENSE ${docsContentPath}
+
+# Copy all immediate children of the markdown output the guides/ directory.
+for guidePath in $(find ${docsDistPath}/markdown/ -maxdepth 1 -type f); do
+  cp ${guidePath} ${docsContentPath}/guides
+done
+
+# All files that aren't immediate children of the markdown output are overview documents.
+for overviewPath in $(find ${docsDistPath}/markdown/ -mindepth 2 -type f); do
+  cp ${overviewPath} ${docsContentPath}/overview
+done
+
+echo "Successfully copied all content into the docs-content repository."
+
+if [[ $(git ls-remote origin "refs/tags/${buildTagName}") ]]; then
+  echo "Skipping publish of docs-content because tag is already published. Exiting.."
+  exit 0
+fi
+
+# Setup the Git configuration
+git config user.name "$commitAuthorName"
+git config user.email "$commitAuthorEmail"
+git config credential.helper "store --file=.git/credentials"
+
+echo "https://${MOSAIC_DOCS_BUILD_TOKEN}:@github.com" > .git/credentials
+
+echo "Credentials for docs-content repository are now set up. Publishing.."
+
+git add -A
+git commit --allow-empty -m "${buildCommitMessage}"
+git tag "${buildTagName}"
+git push origin master --tags
+
+echo "Published docs-content for ${buildVersionName} into ${branchName} successfully"
