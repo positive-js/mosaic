3c0adc22561f129e7b940cf5a3a0cc2c55d05078
evilfant <ifruty@gmail.com>
2018-07-20 20:16:15 +0700

    fix(progress-spinner): add module export (#16)

--- src/lib/public-api.ts
+++ src/lib/public-api.ts
@@ -9,4 +9,5 @@ export * from '@ptsecurity/mosaic/radio';
 export * from '@ptsecurity/mosaic/list';
 export * from '@ptsecurity/mosaic/checkbox';
 export * from '@ptsecurity/mosaic/progress-bar';
+export * from '@ptsecurity/mosaic/progress-spinner';
 export * from '@ptsecurity/mosaic/navbar';
