082e4da4dbccd15a1d7ee942914cacad65f97721
Oleg Pimenov <pimenov.o@gmail.com>
2018-07-24 15:40:54 +0300

    added mc-buttons

--- src/lib/modal/modal.module.ts
+++ src/lib/modal/modal.module.ts
@@ -4,13 +4,16 @@ import { OverlayModule } from '@ptsecurity/cdk/overlay';
 
 import { CssUnitPipe } from '../core/pipes/css-unit.pipe';
 
+import { McButtonModule } from '../button';
+import { McIconModule } from '../icon';
+
 import { McModalControlService } from './modal-control.service';
 import { McModalComponent } from './modal.component';
 import { McModalService } from './modal.service';
 
 
 @NgModule({
-    imports: [ CommonModule, OverlayModule ],
+    imports: [ CommonModule, OverlayModule, McButtonModule, McIconModule ],
     exports: [ McModalComponent ],
     declarations: [ McModalComponent, CssUnitPipe ],
     entryComponents: [ McModalComponent ],
