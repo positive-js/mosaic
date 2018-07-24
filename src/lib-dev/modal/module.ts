9198ccea5647ba4c8e1557946b20763d8b4a1f45
Oleg Pimenov <pimenov.o@gmail.com>
2018-07-24 15:52:04 +0300

    added example with component

--- src/lib-dev/modal/module.ts
+++ src/lib-dev/modal/module.ts
@@ -1,4 +1,4 @@
-import {Component, NgModule, TemplateRef, ViewEncapsulation} from '@angular/core';
+import {Component, Input, NgModule, TemplateRef, ViewEncapsulation} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -53,6 +53,34 @@ export class ModalDemoComponent {
         });
     }
 
+    createComponentModal(): void {
+        const modal = this.modalService.create({
+            mcTitle: 'Modal Title',
+            mcContent: McModalCustomComponent,
+            mcComponentParams: {
+                title: 'title in component',
+                subtitle: 'component sub title，will be changed after 2 sec'
+            },
+            mcFooter: [{
+                label: 'change component tilte from outside',
+                onClick: (componentInstance: any) => {
+                    componentInstance.title = 'title in inner component is changed';
+                }
+            }]
+        });
+
+        modal.afterOpen.subscribe(() => console.log('[afterOpen] emitted!'));
+
+        // Return a result when closed
+        modal.afterClose.subscribe((result) => console.log('[afterClose] The result is:', result));
+
+        // delay until modal instance created
+        window.setTimeout(() => {
+            const instance = modal.getContentComponent();
+            instance.subtitle = 'sub title is changed';
+        }, 2000);
+    }
+
     destroyTplModal(): void {
         this.tplModal.destroy();
     }
@@ -73,9 +101,37 @@ export class ModalDemoComponent {
 }
 
 
+@Component({
+    selector: 'mc-modal-custom-component',
+    template: `
+    <div>
+      <h2>{{ title }}</h2>
+      <h4>{{ subtitle }}</h4>
+      <p>
+        <span>Get Modal instance in component</span>
+        <button mc-button color="primary" (click)="destroyModal()">destroy modal in the component</button>
+      </p>
+    </div>
+  `
+})
+export class McModalCustomComponent {
+    @Input() title: string;
+    @Input() subtitle: string;
+
+    constructor(private modal: McModalRef) { }
+
+    destroyModal(): void {
+        this.modal.destroy({ data: 'this the result data' });
+    }
+}
+
 @NgModule({
     declarations: [
-        ModalDemoComponent
+        ModalDemoComponent,
+        McModalCustomComponent
+    ],
+    entryComponents: [
+        McModalCustomComponent
     ],
     imports: [
         BrowserModule,
