2456dcb45a160460de7766400228e036870acaf0
Oleg Pimenov <pimenov.o@gmail.com>
2018-08-05 22:40:03 +0300

    feat(build): added madge lint

--- tools/gulp/tasks/lint.ts
+++ tools/gulp/tasks/lint.ts
@@ -1,10 +1,42 @@
-import { task } from 'gulp';
+import * as chalk from 'chalk';
 
+import { task } from 'gulp';
+import { join } from 'path';
+
+import { buildConfig } from '../../packages';
 import { execNodeTask } from '../utils/helpers';
 
 
+/* tslint:disable:no-console */
+/* tslint:disable:no-var-requires */
+const madge = require('madge');
+
 const tsLintBaseFlags = ['-c', 'tslint.json', '--project', './tsconfig.json'];
 
-task('lint', ['tslint']);
+/** Path to the output of the Mosaic package. */
+const mosaicOutPath = join(buildConfig.outputDir, 'packages', 'mosaic');
+
+/** Path to the output of the CDK package. */
+const cdkOutPath = join(buildConfig.outputDir, 'packages', 'cdk');
+
+task('lint', ['tslint', 'madge']);
 
 task('tslint', execNodeTask('tslint', tsLintBaseFlags));
+
+task('madge', ['mosaic:clean-build'], () => {
+    madge([mosaicOutPath, cdkOutPath]).then((res: any) => {
+        const circularModules = res.circular();
+
+        if (circularModules.length) {
+            console.error();
+            console.error(chalk.default.red(`Madge found modules with circular dependencies.`));
+            console.error(formatMadgeCircularModules(circularModules));
+            console.error();
+        }
+    });
+});
+
+/** Returns a string that formats the graph of circular modules. */
+function formatMadgeCircularModules(circularModules: string[][]): string {
+    return circularModules.map((modulePaths: string[]) => `\n - ${modulePaths.join(' > ')}`).join('');
+}
