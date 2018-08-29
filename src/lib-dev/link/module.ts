06c32292fd7baf3f69d8a76c50b8c14e36c4e0ab
INikolaev <inikolaev@ptsecurity.com>
2018-08-29 09:20:38 +0300

    feat(link): add link component(#151591)

---
+++ src/lib-dev/link/module.ts
@@ -0,0 +1,36 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { McIconModule } from '../../lib/icon';
+import { McLinkModule } from '../../lib/link';
+
+
+@Component({
+    selector: 'app',
+    template: require('./template.html'),
+    styleUrls: ['./styles.scss']
+})
+export class DemoComponent {}
+
+
+/* tslint:disable:max-classes-per-file */
+@NgModule({
+    declarations: [
+        DemoComponent
+    ],
+    imports: [
+        BrowserModule,
+        McLinkModule,
+        McIconModule
+    ],
+    bootstrap: [
+        DemoComponent
+    ]
+})
+export class DemoModule {}
+
+platformBrowserDynamic()
+    .bootstrapModule(DemoModule)
+    /* tslint:disable:no-console */
+    .catch((error) => console.error(error));
