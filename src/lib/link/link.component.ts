cf218fb112a5048905ffc4509bdf9c6e2b3463b3
INikolaev <inikolaev@ptsecurity.com>
2018-09-04 11:35:49 +0300

    fix(link): rework attributes

--- src/lib/link/link.component.ts
+++ src/lib/link/link.component.ts
@@ -1,28 +1,72 @@
-import { Component, ElementRef, OnDestroy, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+    Input,
+    Attribute,
+    Component,
+    ElementRef,
+    OnDestroy,
+    ViewEncapsulation,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef
+} from '@angular/core';
 
 import { FocusMonitor } from '@ptsecurity/cdk/a11y';
+import {
+    CanDisable,
+    HasTabIndex,
+    mixinDisabled,
+    mixinTabIndex,
+    toBoolean
+} from '@ptsecurity/mosaic/core';
 
+
+export class McLinkBase {
+    constructor(public _elementRef: ElementRef) {
+    }
+}
+
+export const _McLinkBase = mixinTabIndex(mixinDisabled(McLinkBase));
 
 @Component({
     selector: 'a.mc-link',
     template: `<ng-content></ng-content>`,
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
+    exportAs: 'mcLink',
     styleUrls: ['./link.css'],
-    inputs: ['disabled', 'tabindex'],
+    inputs: ['disabled'],
     host: {
         '[attr.disabled]': 'disabled || null',
-        '[attr.tabindex]': 'disabled ? -1 : tabindex ? tabindex : 0'
+        '[attr.tabindex]': 'tabIndex'
     }
 })
 
-export class McLink implements OnDestroy {
-    disabled: boolean;
-    tabindex: number;
+export class McLink extends _McLinkBase implements OnDestroy, HasTabIndex, CanDisable {
 
-    constructor(private elementRef: ElementRef, private _focusMonitor: FocusMonitor) {
+    @Input()
+    get disabled() {
+        return this._disabled;
+    }
 
+    set disabled(value: any) {
+        const newValue = toBoolean(value);
+
+        if (newValue !== this._disabled) {
+            this._disabled = newValue;
+            this._changeDetector.markForCheck();
+        }
+    }
+
+    private _disabled = false;
+
+    constructor(
+        @Attribute('tabindex') tabIndex: string,
+        public elementRef: ElementRef,
+        private _focusMonitor: FocusMonitor,
+        private _changeDetector: ChangeDetectorRef) {
+
+        super(elementRef);
         this._focusMonitor.monitor(elementRef.nativeElement, true);
+        this.tabIndex = parseInt(tabIndex) || 0;
     }
 
     ngOnDestroy() {
