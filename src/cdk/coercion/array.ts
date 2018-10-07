96d5f07f96732c0a2cad63a0b8b3aa80233e95c6
Oleg Pimenov <pimenov.o@gmail.com>
2018-10-07 10:45:34 +0300

    chore(cdk): updated tslint

--- src/cdk/coercion/array.ts
+++ src/cdk/coercion/array.ts
@@ -1,5 +1,4 @@
-
 /** Wraps the provided value in an array, unless the provided value is an array. */
 export function coerceArray<T>(value: T | T[]): T[] {
-  return Array.isArray(value) ? value : [value];
+    return Array.isArray(value) ? value : [value];
 }
