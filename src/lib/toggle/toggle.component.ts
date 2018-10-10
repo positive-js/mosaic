31a0ebe18afd5e2a619b6d78886dfac6a70e25c1
Peter Kornuishin <pkornyushin@ptsecurity.com>
2018-10-10 16:12:38 +0300

    base theme and contaner

--- src/lib/toggle/toggle.component.ts
+++ src/lib/toggle/toggle.component.ts
@@ -1,10 +1,35 @@
-import { Directive } from '@angular/core';
+// tslint:disable:no-empty
+import { Component } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 
-@Directive({
+@Component({
     selector: 'mc-toggle',
-    exportAs: 'mcToggle'
+    exportAs: 'mcToggle',
+    templateUrl: './toggle.component.html',
+    styleUrls: ['./toggle.css'],
+    providers: [
+        {provide: NG_VALUE_ACCESSOR, useExisting: McToggleComponent, multi: true}
+    ]
 })
-export class McToggleComponent {
+export class McToggleComponent implements ControlValueAccessor {
+    private _value: boolean = true;
 
+    writeValue(value: boolean) {
+        if (value !== this._value) {
+            this._value = value;
+        }
+    }
+
+    registerOnChange(fn: any) {
+        this.onChangeCallback = fn;
+    }
+
+    registerOnTouched(fn: any) {
+        this.onTouchedCallback = fn;
+    }
+
+    private onTouchedCallback = () => {};
+
+    private onChangeCallback = (_: any) => {};
 }
