e38ef4ffa6125767553212237b3ecaaf128946c6
Peter Kornuishin <pkornyushin@ptsecurity.com>
2018-10-10 15:26:53 +0300

    initial commit

---
+++ src/lib/toggle/toggle.component.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+
+@Directive({
+    selector: 'mc-toggle',
+    exportAs: 'mcToggle'
+})
+export class McToggleComponent {
+
+}
---
+++ src/lib/toggle/toggle.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { A11yModule } from '@ptsecurity/cdk/a11y';
+import { McCommonModule } from '@ptsecurity/mosaic/core';
+
+import { McToggleComponent } from './toggle.component';
+
+
+@NgModule({
+    imports: [CommonModule, A11yModule, McCommonModule],
+    exports: [McToggleComponent],
+    declarations: [McToggleComponent]
+})
+export class McToggleModule {
+}
