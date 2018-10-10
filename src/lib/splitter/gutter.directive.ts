9fe4e7c557721a72142fb1136596578d18b3def6
Andrey Rodkin <arodkin@ptsecurity.com>
2018-10-10 10:17:30 +0300

    feat(visual): default gutter image

--- src/lib/splitter/gutter.directive.ts
+++ src/lib/splitter/gutter.directive.ts
@@ -12,6 +12,14 @@ import { Cursor, Direction, State } from "./splitter.constants";
 export class McGutterDirective {
     private _direction: Direction = Direction.Vertical;
     private _disabled: boolean = false;
+    private _horizontalImage: string = 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg==")';
+    private _verticalImage: string = 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAFCAMAAABl/6zIAAAABlBMVEUAAADMzMzIT8AyAAAAAXRSTlMAQObYZgAAABRJREFUeAFjYGRkwIMJSeMHlBkOABP7AEGzSuPKAAAAAElFTkSuQmCC")';
+    private _withAnimation: boolean = false;
+
+    @Input()
+    set color(color: string) {
+        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', color ? color : '#eeeeee');
+    }
 
     @Input()
     set direction(direction: Direction) {
@@ -34,6 +42,20 @@ export class McGutterDirective {
     }
 
     @Input()
+    set horizontalImage(image: string) {
+        this._horizontalImage = image;
+    }
+
+    get horizontalImage(): string {
+        return this._horizontalImage;
+    }
+
+    @Input()
+    set order(order: number) {
+        this.renderer.addClass(this.elementRef.nativeElement, `flex-order-${order}`);
+    }
+
+    @Input()
     set size(size: number) {
         this.renderer.setStyle(
             this.elementRef.nativeElement,
@@ -45,8 +67,27 @@ export class McGutterDirective {
     }
 
     @Input()
-    set order(order: number) {
-        this.renderer.addClass(this.elementRef.nativeElement, `flex-order-${order}`);
+    set verticalImage(image: string) {
+        this._verticalImage = image;
+    }
+
+    get verticalImage(): string {
+        return this._verticalImage;
+    }
+
+    @Input()
+    set withAnimation(withAnimation: boolean) {
+        this._withAnimation = withAnimation;
+
+        if (this.withAnimation) {
+            this.renderer.setStyle(this.elementRef.nativeElement, 'transition', 'flex-basis 0.3s');
+        } else {
+            this.renderer.removeStyle(this.elementRef.nativeElement, 'transition');
+        }
+    }
+
+    get withAnimation(): boolean {
+        return this._withAnimation;
     }
 
     constructor(private renderer: Renderer2,
@@ -55,14 +96,6 @@ export class McGutterDirective {
 
     private isVertical(): boolean {
         return this.direction === Direction.Vertical;
-    }
-
-    private getState(): State {
-        return this.disabled
-            ? State.Disabled
-            : this.direction === Direction.Vertical
-                ? State.Vertical
-                : State.Horizontal;
     }
 
     private getCursor(state: State): string {
@@ -74,11 +107,33 @@ export class McGutterDirective {
             case State.Horizontal:
                 return Cursor.ResizeColumn;
             default:
-                throw Error(`Unknown gutter state: ${state}`);
+                throw Error(`Unknown gutter state for cursor: ${state}`);
         }
+    }
+
+    private  getImage(state: State): string {
+        switch (state) {
+            case State.Disabled:
+                return '';
+            case State.Vertical:
+                return this.verticalImage;
+            case State.Horizontal:
+                return this.horizontalImage;
+            default:
+                throw Error(`Unknown gutter state for image: ${state}`);
+        }
+    }
+
+    private getState(): State {
+        return this.disabled
+            ? State.Disabled
+            : this.direction === Direction.Vertical
+                ? State.Vertical
+                : State.Horizontal;
     }
 
     private refresh(): void {
         this.renderer.setStyle(this.elementRef.nativeElement, 'cursor', this.getCursor(this.getState()));
+        this.renderer.setStyle(this.elementRef.nativeElement, 'background-image', this.getImage((this.getState())));
     }
 }
