20bc0814750a0d6e454baf9695d3c3b232601096
Andrey Rodkin <arodkin@ptsecurity.com>
2018-10-15 13:56:40 +0300

    feat(splitter): fix icon import

--- src/lib/splitter/splitter.module.ts
+++ src/lib/splitter/splitter.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { McIconModule } from '../icon/icon.module';
+import { McIconModule } from '@ptsecurity/mosaic/icon';
+
 import { McGutterDirective } from '../splitter/gutter.directive';
 import { McSplitterAreaDirective } from '../splitter/splitter-area.directive';
 import { McSplitterComponent } from '../splitter/splitter.component';
