dffeeec06bb09014f3ad0770357c12019526caa0
Marenkov Vadim Viktorovich <Dangel84@users.noreply.github.com>
2018-11-01 19:40:02 +0700

    feat(cards): Added cards (#48)

---
+++ src/lib-dev/card/module.ts
@@ -0,0 +1,42 @@
+import { Component, NgModule, ViewEncapsulation } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { McCardModule, Status } from '../../lib/card';
+
+
+@Component({
+    selector: 'app',
+    template: require('./template.html'),
+    styleUrls: ['./styles.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class CardsDemoComponent {
+    state = Status;
+
+    s1 = false;
+    s2 = false;
+    s3 = false;
+    s4 = false;
+}
+
+
+@NgModule({
+    declarations: [
+        CardsDemoComponent
+    ],
+    imports: [
+        BrowserModule,
+        McCardModule
+    ],
+    bootstrap: [
+        CardsDemoComponent
+    ]
+})
+export class CardsDemoModule {}
+
+platformBrowserDynamic()
+    .bootstrapModule(CardsDemoModule)
+    // tslint:disable-next-line
+    .catch((error) => console.error(error));
+
