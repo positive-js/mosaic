8f276e602a775283b7e921b695ed34f9eaaf5539
Peter Kornuishin <pkornyushin@ptsecurity.com>
2018-11-13 10:24:33 +0300

    karma config changed

--- tests/karma-system-config.js
+++ tests/karma-system-config.js
@@ -65,6 +65,7 @@ System.config({
         '@ptsecurity/mosaic/modal': 'dist/packages/mosaic/modal/index.js',
         '@ptsecurity/mosaic/tag': 'dist/packages/mosaic/tag/index.js',
         '@ptsecurity/mosaic/select': 'dist/packages/mosaic/select/index.js',
+        '@ptsecurity/mosaic/textarea': 'dist/packages/mosaic/textarea/index.js',
         '@ptsecurity/mosaic/tooltip': 'dist/packages/mosaic/tooltip/index.js',
         '@ptsucurity/mosaic/timepicker': 'dist/packages/mosaic/timepicker/index.js',
         '@ptsecurity/mosaic/splitter': 'dist/packages/mosaic/splitter/index.js'
