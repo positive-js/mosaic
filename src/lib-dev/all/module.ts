80b4ee122d2f8bad150f93efe64640ce819c2e40
lskramarov <lskramarov@users.noreply.github.com>
2018-12-17 10:19:58 +0300

    Feature/dark theme in link and other (#75)
    * feat(dark-theme): Link (#167921)
    
    * feat(dark-theme): Tooltip (#167971)
    
    * feat(dark-theme): Modal (#167922)
    
    * feat(dark-theme): panel, progress-bar, splitter (#168268)
    
    * created dev example with all components
    
    * feat(dark-theme): Badge (#168321)
    
    * added command for run dev example with all components
    
    * fixed some notes after review

---
+++ src/lib-dev/all/module.ts
@@ -0,0 +1,228 @@
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+// tslint:disable:no-console
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { Component, NgModule, ViewEncapsulation } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { CdkTreeModule, FlatTreeControl, NestedTreeControl } from '@ptsecurity/cdk/tree';
+import { McButtonModule } from '@ptsecurity/mosaic/button';
+
+import { McCardModule } from '@ptsecurity/mosaic/card';
+import { McCheckboxModule } from '@ptsecurity/mosaic/checkbox';
+import { McDropdownModule } from '@ptsecurity/mosaic/dropdown';
+import { McFormFieldModule } from '@ptsecurity/mosaic/form-field';
+import { McIconModule } from '@ptsecurity/mosaic/icon';
+import { McInputModule } from '@ptsecurity/mosaic/input';
+import { McLayoutModule } from '@ptsecurity/mosaic/layout';
+import { McLinkModule } from '@ptsecurity/mosaic/link';
+import { McListModule } from '@ptsecurity/mosaic/list';
+import { McModalModule, McModalService } from '@ptsecurity/mosaic/modal';
+import { McNavbarModule } from '@ptsecurity/mosaic/navbar';
+import { McProgressBarModule } from '@ptsecurity/mosaic/progress-bar';
+import { McProgressSpinnerModule } from '@ptsecurity/mosaic/progress-spinner';
+import { McRadioModule } from '@ptsecurity/mosaic/radio';
+import { McSelectModule } from '@ptsecurity/mosaic/select';
+import { McSplitterModule } from '@ptsecurity/mosaic/splitter';
+import { McTagModule } from '@ptsecurity/mosaic/tag';
+import { McTextareaModule } from '@ptsecurity/mosaic/textarea';
+import { McTimepickerModule } from '@ptsecurity/mosaic/timepicker';
+import { McToggleModule } from '@ptsecurity/mosaic/toggle';
+import { McToolTipModule } from '@ptsecurity/mosaic/tooltip';
+import { McTreeFlatDataSource, McTreeFlattener, McTreeModule, McTreeNestedDataSource } from '@ptsecurity/mosaic/tree';
+import { Observable, of as observableOf } from 'rxjs';
+
+import { FileDatabase, FileFlatNode, FileNode } from '../tree/module';
+
+
+const INTERVAL: number = 300;
+const STEP: number = 4;
+const MAX_PERCENT: number = 100;
+
+
+@Component({
+    selector: 'app',
+    template: require('./template.html'),
+    styleUrls: ['./styles.scss'],
+    encapsulation: ViewEncapsulation.None,
+    providers: [FileDatabase]
+})
+export class DemoComponent {
+    checked: boolean[] = [true, true, false];
+    indeterminate: boolean = true;
+    disabled: boolean = false;
+    labelPosition = 'after';
+
+    typesOfShoes = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+
+    folders = [
+        {
+            name: 'Photos',
+            updated: new Date('1/1/16')
+        },
+        {
+            name: 'Recipes',
+            updated: new Date('1/17/16')
+        },
+        {
+            name: 'Work',
+            updated: new Date('1/28/16')
+        }
+    ];
+    notes = [
+        {
+            name: 'Vacation Itinerary',
+            updated: new Date('2/20/16')
+        },
+        {
+            name: 'Kitchen Remodel',
+            updated: new Date('1/18/16')
+        }
+    ];
+
+    mode: string = 'determinate';
+    percent: number = 0;
+    intervalId: number;
+
+    favoriteFruit: string;
+
+    isDisabled: boolean = true;
+
+    fruits = [
+        'Apple',
+        'Banana',
+        'Tomato',
+        'Крякать как уточка'
+    ];
+
+    selectionList = [
+        {name: 'Yes', value: 'true', selected: false},
+        {name: 'No', value: 'false', selected: true}
+    ];
+
+    singleSelected = 'Normal';
+    multipleSelected = ['Normal', 'Hovered', 'Selected', 'Selected1', 'Boots', 'Clogs'];
+
+    singleSelectFormControl = new FormControl('', Validators.required);
+
+    multiSelectSelectFormControl = new FormControl([], Validators.pattern(/^w/));
+
+    timeValue1: Date = new Date();
+
+    treeControl: FlatTreeControl<FileFlatNode>;
+    dataSource: McTreeFlatDataSource<FileNode, FileFlatNode>;
+    treeFlattener: McTreeFlattener<FileNode, FileFlatNode>;
+
+    nestedTreeControl: NestedTreeControl<FileNode>;
+    nestedDataSource: McTreeNestedDataSource<FileNode>;
+
+    constructor(private modalService: McModalService, database: FileDatabase) {
+        setInterval(() => {
+            this.percent = (this.percent + STEP) % (MAX_PERCENT + STEP);
+        }, INTERVAL);
+
+        this.treeFlattener = new McTreeFlattener(
+            this.transformer, this._getLevel, this._isExpandable, this._getChildren
+        );
+
+        this.treeControl = new FlatTreeControl<FileFlatNode>(this._getLevel, this._isExpandable);
+        this.dataSource = new McTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+        this.nestedTreeControl = new NestedTreeControl<FileNode>(this._getChildren);
+        this.nestedDataSource = new McTreeNestedDataSource();
+
+        database.dataChange.subscribe((data) => {
+            this.dataSource.data = data;
+            this.nestedDataSource.data = data;
+        });
+    }
+
+    showConfirm() {
+        this.modalService.success({
+            mcContent   : 'Сохранить сделанные изменения в запросе "Все активы с виндой"?',
+            mcOkText    : 'Сохранить',
+            mcCancelText: 'Отмена',
+            mcOnOk      : () => console.log('OK')
+        });
+    }
+
+    toggleDisable() {
+        this.isDisabled = !this.isDisabled;
+    }
+
+    transformer(node: FileNode, level: number) {
+        const flatNode = new FileFlatNode();
+
+        flatNode.name = node.name;
+        flatNode.type = node.type;
+        flatNode.level = level;
+        flatNode.expandable = !!node.children;
+
+        return flatNode;
+    }
+
+    hasChild(_: number, _nodeData: FileFlatNode) { return _nodeData.expandable; }
+
+    hasNestedChild(_: number, nodeData: FileNode) {
+        return !(nodeData.type);
+    }
+
+    ngOnDestroy() {
+        clearInterval(this.intervalId);
+    }
+
+    private _getLevel(node: FileFlatNode) { return node.level; }
+
+    private _isExpandable(node: FileFlatNode) { return node.expandable; }
+
+    private _getChildren = (node: FileNode): Observable<FileNode[]> => {
+        return observableOf(node.children);
+    }
+
+}
+
+
+@NgModule({
+    declarations: [
+        DemoComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        McIconModule,
+        McButtonModule,
+        McLinkModule,
+        McCardModule,
+        McCheckboxModule,
+        McDropdownModule,
+        McInputModule,
+        McFormFieldModule,
+        McLayoutModule,
+        McNavbarModule,
+        McListModule,
+        McModalModule,
+        McProgressBarModule,
+        McProgressSpinnerModule,
+        McRadioModule,
+        McSelectModule,
+        McSplitterModule,
+        McTagModule,
+        McTextareaModule,
+        McTimepickerModule,
+        McToggleModule,
+        McToolTipModule,
+        McTreeModule,
+        CdkTreeModule
+    ],
+    bootstrap: [
+        DemoComponent
+    ]
+})
+export class DemoModule {}
+
+platformBrowserDynamic()
+    .bootstrapModule(DemoModule)
+    .catch((error) => console.error(error));
--- src/lib-dev/modal/module.ts
+++ src/lib-dev/modal/module.ts
@@ -37,7 +37,7 @@ export class ModalDemoComponent {
             mcContent   : 'The selected action "Send to Arbor" is used in a rule' +
                 ' or an alert. It will be <b>deleted</b> there too. </br></br>' +
                 'Delete the selected action anyway?',
-            mcOkType    : 'warn',
+            mcOkType    : 'error',
             mcOkText    : 'Yes',
             mcCancelText: 'No',
             mcWidth     : '480px',
