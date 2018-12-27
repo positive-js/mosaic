914b2b6a69973bb744fb8f49a0a11d5d04b7e10e
Oleg Pimenov <pimenov.o@gmail.com>
2018-12-27 18:04:23 +0300

    chore: updated release scripts, fix tsconfig

--- tools/release/publish-release.ts
+++ tools/release/publish-release.ts
@@ -63,7 +63,7 @@ class PublishReleaseTask extends BaseReleaseTask {
     async run() {
         console.log();
         console.log(chalk.green('-----------------------------------------'));
-        console.log(chalk.green(chalk.bold('  Angular Material release script')));
+        console.log(chalk.green(chalk.bold('  Mosaic release script')));
         console.log(chalk.green('-----------------------------------------'));
         console.log();
 
