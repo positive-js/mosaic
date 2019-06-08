b9c801878a3c1730dde6f906bd6d2883c5e29f97
Oleg Pimenov <pimenov.o@gmail.com>
2019-06-08 13:57:36 +0300

    refactor(overlay): rm explicit subscription count

--- commitlint.config.js
+++ commitlint.config.js
@@ -20,6 +20,7 @@ module.exports = {
                 'datepicker',
                 'docs',
                 'dropdown',
+                'overlay',
                 'form-field',
                 'icon',
                 'input',
