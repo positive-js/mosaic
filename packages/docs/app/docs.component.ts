d49d27ef652da2b3e0d62ed7aa98034870855cd3
Oleg Pimenov <pimenov.o@gmail.com>
2019-06-12 16:36:34 +0300

    chore: init docs

---
+++ packages/docs/app/docs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'docs-app',
+    templateUrl: './docs.template.html',
+    styleUrls: ['./docs.scss']
+})
+export class DocsComponent {
+
+}
