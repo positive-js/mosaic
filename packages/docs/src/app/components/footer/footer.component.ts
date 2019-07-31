fc47b6d19772934e86c4aba1d332bab52f4e731f
myakovleva <myakovleva@ptsecurity.com>
2019-07-31 16:02:01 +0300

    feat(docs): footer import was corrected
    feat(docs): footer import was corrected
    
    footer import was corrected
    
    Breaking Changes: none

--- packages/docs/src/app/components/footer/footer.component.ts
+++ packages/docs/src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
     selector: 'footer',
     templateUrl: './footer.component.html',
--- packages/docs/src/app/components/footer/footer.module.ts
+++ packages/docs/src/app/components/footer/footer.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
 import { FooterComponent } from './footer.component';
+
 
 @NgModule({
     imports: [CommonModule],
