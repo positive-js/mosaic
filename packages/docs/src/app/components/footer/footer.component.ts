bf24c7a33164c7cbc2bd9f89806f113d49dfa140
myakovleva <myakovleva@ptsecurity.com>
2019-07-31 12:35:28 +0300

    feat(docs): footer was added
    feat(docs): footer was added
    
    footer was added
    
    Breaking Changes: none

---
+++ packages/docs/src/app/components/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
+})
+
+export class FooterComponent {}
---
+++ packages/docs/src/app/components/footer/footer.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FooterComponent } from './footer.component';
+
+@NgModule({
+    imports: [CommonModule],
+    exports: [FooterComponent],
+    declarations: [FooterComponent]
+})
+export class FooterModule {}
