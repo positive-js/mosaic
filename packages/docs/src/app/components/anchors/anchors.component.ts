87d69da26d1cb15726ca4cd0b1f819c8121f42d7
myakovleva <myakovleva@ptsecurity.com>
2019-08-05 14:57:38 +0300

    feat(docs): anchor's automatic creation was added
    feat(docs): anchor's automatic creation was added
    
    anchor's automatic creation was added
    
    Breaking Changes: none

--- packages/docs/src/app/components/anchors/anchors.component.ts
+++ packages/docs/src/app/components/anchors/anchors.component.ts
@@ -1,6 +1,20 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { ChangeDetectorRef , Component, ElementRef, Inject, Input } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Subject, fromEvent } from 'rxjs';
+import { debounceTime, map, takeUntil } from 'rxjs/operators';
 
+
+interface IAnchor {
+    href: string;
+    name: string;
+    /* header level h3/h4 */
+    level: string;
+    /* If the anchor is in view of the page */
+    active: boolean;
+    /* top offset px of the anchor */
+    top: number;
+}
 
 @Component({
     selector: 'anchors',
@@ -9,32 +23,151 @@ import { Router } from '@angular/router';
 })
 export class AnchorsComponent {
 
-    activeAnchor: number = 0;
-    activeClass: string = "anchors-menu__list-element anchors-menu__list-element_active";
-    anchors: {href: string, name: string }[];
-    inactiveClass: string = "anchors-menu__list-element";
-    href: any;
+    @Input() anchors: IAnchor[] = [];
+    @Input() headerSelectors = 'h3.docs-header-link, h4.docs-header-link'; // TODO edit selector to right one
 
-    constructor(private router : Router) {}
+    click: boolean = false;
+    container: string;
+    headerHeight: number = 64;
+    debounceTime: number = 20;
+    private destroyed = new Subject();
+    private urlFragment = '';
+    private scrollContainer: any;
+    private  currentUrl: any;
+
+    constructor(private router: Router,
+                private route: ActivatedRoute,
+                private element: ElementRef,
+                private ref: ChangeDetectorRef,
+                @Inject(DOCUMENT) private document: Document) {
+
+        this.container = '.anchors-menu';
+
+        this.router.events.pipe(takeUntil(this.destroyed)).subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                const rootUrl = router.url.split('#')[0];
+                if (rootUrl !== this.currentUrl) {
+                    this.anchors = this.createAnchors();
+                    this.currentUrl = rootUrl;
+                }
+            }
+        });
+
+        this.route.fragment.pipe(takeUntil(this.destroyed)).subscribe((fragment) => {
+            this.urlFragment = fragment;
+            const target = document.getElementById(this.urlFragment);
+
+            if (target) {
+                this.click = true;
+                target.scrollIntoView();
+            }
+
+        });
+    }
 
     ngAfterViewInit() {
-        // TODO: collect real anchors data
+        this.updateScrollPosition();
     }
 
-    ngOnInit() {
-        this.href = this.removeAnchor(this.router.url);
-        this.anchors = [
-            {href: `${this.href}#variants`, name: "Variants"},
-            {href: `${this.href}#colors`, name: "Colors"},
-            {href: `${this.href}#progress-indication`, name: "Progress indication"}
-        ];
+    ngOnDestroy() {
+        this.destroyed.next();
     }
 
-    setActiveAnchor(index) {
-        this.activeAnchor = index;
+    updateScrollPosition() {
+        this.anchors = this.createAnchors();
+        const target = document.getElementById(this.urlFragment);
+        if (target) {
+            target.scrollIntoView();
+        }
     }
 
-    removeAnchor(url) {
-        return url.split('#')[0];
+    setScrollPosition() {
+        this.anchors = this.createAnchors();
+        const target = document.getElementById(this.urlFragment);
+        if (target) {
+            target.scrollIntoView();
+        }
+
+        Promise.resolve().then(() => {
+            this.scrollContainer = this.document || window;
+
+            if (this.scrollContainer) {
+                fromEvent(this.scrollContainer, 'scroll').pipe(
+
+                    takeUntil(this.destroyed),
+                    debounceTime(this.debounceTime))
+                    .subscribe(() => this.onScroll());
+            }
+        });
+    }
+
+    onResize() {
+        const headers = Array.from(this.document.querySelectorAll(this.headerSelectors)) as HTMLElement[];
+
+        for (let i = 0; i < this.anchors.length; i++) {
+            const {top} = headers[i].getBoundingClientRect();
+            this.anchors[i].top = top;
+        }
+
+        this.ref.detectChanges();
+    }
+
+    /** Gets the scroll offset of the scroll container */
+    private getScrollOffset(): number {
+        return window.pageYOffset + this.headerHeight;
+    }
+
+    private createAnchors(): IAnchor[] {
+        const anchors = [];
+        const headers = Array.from(this.document.querySelectorAll(this.headerSelectors)) as HTMLElement[];
+
+        if (headers.length) {
+            for (let i = 0; i < headers.length; i++) {
+                // remove the 'link' icon name from the inner text
+                const name = headers[i].innerText.trim().replace(/^link/, '');
+                const href = `#${headers[i].id}`;
+                const {top} = headers[i].getBoundingClientRect();
+
+                anchors.push({
+                    href,
+                    name,
+                    level: headers[i].tagName.toLowerCase() === 'h4',
+                    active: i === 0,
+                    top
+                });
+            }
+        }
+
+        return anchors;
+    }
+
+    private onScroll() {
+        if (this.click) {
+            this.click = false;
+
+            return;
+        }
+
+        for (let i = 0; i < this.anchors.length; i++) {
+            this.anchors[i].active = this.isLinkActive(this.anchors[i], this.anchors[i + 1]);
+        }
+
+        this.ref.detectChanges();
+    }
+
+    private isLinkActive(currentLink: any, nextLink: any): boolean {
+        // A link is considered active if the page is scrolled passed the anchor without also
+        // being scrolled passed the next link
+        const scrollOffset = this.getScrollOffset();
+
+        return scrollOffset >= currentLink.top && !(nextLink && nextLink.top < scrollOffset);
+    }
+
+    private setActiveAnchor(index) {
+        for (const anchor of this.anchors) {
+            anchor.active = false;
+        }
+        this.anchors[index].active = true;
+        this.ref.detectChanges();
     }
 }
