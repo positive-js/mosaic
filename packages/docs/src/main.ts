db42da2f4d250f6675cfa1934e0cbea2ccbde76c
Oleg Pimenov <opimenov@ptsecurity.com>
2019-08-07 16:05:08 +0300

    chore: adde sevice workers

--- packages/docs/src/main.ts
+++ packages/docs/src/main.ts
@@ -5,7 +5,13 @@ import { AppModule } from './app/';
 import { environment } from './environments/environment';
 // tslint:disable-next-line:no-import-side-effect
 import './polyfills.ts';
+import { unregisterServiceWorkers } from './unregister-service-workers';
 
+
+// Unregister all installed service workers and force reload the page if there was
+// an old service worker from a previous version of the docs.
+unregisterServiceWorkers()
+    .then(hadServiceWorker => hadServiceWorker && location.reload(true));
 
 // tslint:disable-next-line:blank-lines
 if (environment.production) {
