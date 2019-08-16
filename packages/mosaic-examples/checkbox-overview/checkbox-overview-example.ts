211784f306ff7e0581ec1b8fe5bd285d00ecd633
Oleg Pimenov <pimenov.o@gmail.com>
2019-08-16 18:02:02 +0300

    chore: added checkbox example

---
+++ packages/mosaic-examples/checkbox-overview/checkbox-overview-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Basic checkboxes
+ */
+@Component({
+    selector: 'checkbox-overview-example',
+    templateUrl: 'checkbox-overview-example.html',
+    styleUrls: ['checkbox-overview-example.css']
+})
+export class CheckboxOverviewExample {}
