36e10d9b405475a2aaed5feeabd09440f7efc59a
myakovleva <myakovleva@ptsecurity.com>
2019-08-26 18:42:56 +0300

    feat(docs): checkbox documentation
    feat(docs): checkbox documentation
    
    checkbox documentation and examples updated
    
    Breaking Changes: none

---
+++ packages/mosaic-examples/checkbox-indeterminate/checkbox-indeterminate-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Indeterminate checkbox
+ */
+@Component({
+    selector: 'checkbox-indeterminate-example',
+    templateUrl: 'checkbox-indeterminate-example.html',
+    styleUrls: ['checkbox-indeterminate-example.css']
+})
+export class CheckboxIndeterminateExample {}
