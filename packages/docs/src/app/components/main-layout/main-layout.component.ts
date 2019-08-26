cdeca452587c38524c082a5ea1bb0ba1528fa5bd
Iakovleva Margarita <naruyaoi@yandex.ru>
2019-08-26 13:32:27 +0300

    feat(docs): routing in production (#210)

--- packages/docs/src/app/components/main-layout/main-layout.component.ts
+++ packages/docs/src/app/components/main-layout/main-layout.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -8,10 +8,41 @@ import { Router } from '@angular/router';
 })
 export class MainLayoutComponent {
 
-    constructor(private router: Router) {
+    constructor(private router: Router,
+                private route: ActivatedRoute) {
+
+        this.setNextRoute();
+
         if (this.router.routerState.snapshot.url === '/') {
             this.router.navigateByUrl('button/overview');
         }
+    }
+
+    setNextRoute() {
+        const search =  this.getParams();
+        if (!search || !search.nextRoute) { return; }
+        const nextRoute = search.nextRoute;
+        delete search.nextRoute;
+        const route = nextRoute + this.setParams(search) + location.hash;
+        this.router.navigateByUrl(route.split(',').join('/'));
+    }
+
+    getParams() {
+        const search = location.search.substring(1);
+
+        if (search) {
+            return JSON.parse(`{"${decodeURI(search).replace(/"/g, '\\"')
+                .replace(/&/g, '","')
+                .replace(/=/g, '":"')}"}`);
+        }
+    }
+
+    setParams(search) {
+        if (Object.keys(search).length) {
+            return `?${JSON.stringify(search).replace(/","/g, '&').replace(/":"/g, '=').slice(2,-2)}`;
+        }
+
+        return '';
     }
 
 }
