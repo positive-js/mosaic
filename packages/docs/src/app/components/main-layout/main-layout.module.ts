331b463c35a98e681aad0fb3537b62cd689f9eeb
Iakovleva Margarita <naruyaoi@yandex.ru>
2019-09-03 14:55:32 +0300

    feat(docs): angular anchors (#223)

--- packages/docs/src/app/components/main-layout/main-layout.module.ts
+++ packages/docs/src/app/components/main-layout/main-layout.module.ts
@@ -1,4 +1,3 @@
-import { AnchorsModule } from '../anchors/anchors.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -7,11 +6,12 @@ import { McDropdownModule } from '@ptsecurity/mosaic/dropdown';
 import { McIconModule } from '@ptsecurity/mosaic/icon';
 import { McTreeModule } from '@ptsecurity/mosaic/tree';
 
+import { AnchorsModule } from '../anchors/anchors.module';
+import { FooterModule } from '../footer/footer.module';
 import { NavbarModule } from '../navbar';
 import { SidenavModule } from '../sidenav/sidenav.module';
 
 import { MainLayoutComponent } from './main-layout.component';
-import { FooterModule } from '../footer/footer.module';
 
 
 @NgModule({
