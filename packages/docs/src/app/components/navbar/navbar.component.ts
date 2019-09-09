bc9657a554202d97fd310b886305969f1f14370d
Iakovleva Margarita <naruyaoi@yandex.ru>
2019-09-09 19:00:51 +0300

    feat(docs): version dropdown styling (#237)

--- packages/docs/src/app/components/navbar/navbar.component.ts
+++ packages/docs/src/app/components/navbar/navbar.component.ts
@@ -8,21 +8,27 @@ import { Component } from '@angular/core';
 })
 
 export class NavbarComponent {
+    // TODO fetch real data instead
+    versions = [{number: '5.1', date: '15 октября', bold: true},
+        {number: '5.0.1', date: '14 октября', bold: false},
+        {number: '5', date: '13 октября', bold: true},
+        {number: '4.8.8', date: '12 октября', bold: false},
+        {number: '4.8.5', date: '11 октября', bold: false},
+        {number: '4.8.4', date: '10 октября', bold: false},
+        {number: '4.8', date: '9 октября', bold: true},
+        {number: '4.7.1', date: '8 октября', bold: false},
+        {number: '1.0', date: '7 октября', bold: true}];
 
-    versions = ["5.1", "5.0.1", "5", "4.8.8", "4.8.5", "4.8.4", "4.8", "4.7.1", "1.0" ];
-    curVerIndex = this.versions[0];
-
-    languages = ["Русский язык", "Английский язык"];
+    curVerIndex = this.versions[0].number;
+    languages = ['Русский язык', 'Английский язык'];
     curLanguage = this.languages[0];
-
-    themes = ["Светлая тема", "Темная тема"];
+    themes = ['Светлая тема', 'Темная тема'];
     curTheme = this.themes[0];
+    // TODO Эти значения временные, надо определиться с постоянными и заменить ими текущие значения.
+    colors = ['#2f80ed', '#333491', '#07804e', '#eaaf00'];
+    activeColor = '#2f80ed';
 
-    colors = ["#2f80ed", "#333491", "#07804e", "#eaaf00"];
-    activeColor = "#2f80ed";
-    //TODO Эти значения временные, надо определиться с постоянными и заменить ими текущие значения.
-
-    iconFont = "20px";
+    iconFont = '20px';
 
     setVersion(version) {
         this.curVerIndex = version;
