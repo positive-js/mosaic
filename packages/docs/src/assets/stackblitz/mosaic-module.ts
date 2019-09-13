95e841253ce02656a2f78f5971cd6a224be76580
Oleg Pimenov <pimenov.o@gmail.com>
2019-09-13 09:00:35 +0300

    chore: added assets for stackblitz

--- packages/docs/src/assets/stackblitz/mosaic-module.ts
+++ packages/docs/src/assets/stackblitz/mosaic-module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { McButtonModule } from '@ptsecurity/mosaic/button';
 import { McCheckboxModule } from '@ptsecurity/mosaic/checkbox';
+import { McIconModule } from '@ptsecurity/mosaic/icon';
 
 
 @NgModule({
     exports: [
         McButtonModule,
-        McCheckboxModule
+        McCheckboxModule,
+        McIconModule
     ]
 })
 export class DemoMosaicModule {
