7823d2686fcca0a3db304264e0735071835a5dd0
Oleg Pimenov <pimenov.o@gmail.com>
2019-10-18 13:04:43 +0300

    chore: fixed raio-button example

--- packages/mosaic-examples/mosaic-module.ts
+++ packages/mosaic-examples/mosaic-module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { McButtonModule } from '@ptsecurity/mosaic/button';
 import { McCheckboxModule } from '@ptsecurity/mosaic/checkbox';
+import { McRadioModule } from '@ptsecurity/mosaic/radio';
 
 
 @NgModule({
 
      imports: [
          McButtonModule,
-         McCheckboxModule
+         McCheckboxModule,
+         McRadioModule
     ],
     exports: [
         McButtonModule,
-        McCheckboxModule
+        McCheckboxModule,
+        McRadioModule
     ]
 })
 export class ExampleMosaicModule {}
---
+++ packages/mosaic-examples/mosaic/radio/radio-overview/radio-overview-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Basic checkboxes
+ */
+@Component({
+    selector: 'radio-overview-example',
+    templateUrl: 'radio-overview-example.html',
+    styleUrls: ['radio-overview-example.css']
+})
+export class RadioOverviewExample {}
