13dac99e16a36c968799488bf57eca58680bc7a6
Iakovleva Margarita <naruyaoi@yandex.ru>
2019-10-29 14:40:17 +0300

    feat(docs): adding dropdown examples (#322)
    Dropdown overview and nested dropdown examples are added.

---
+++ packages/mosaic-examples/mosaic/dropdown/dropdown-nested/dropdown-nested-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Nested dropdown
+ */
+@Component({
+    selector: 'dropdown-nested-example',
+    templateUrl: 'dropdown-nested-example.html',
+    styleUrls: ['dropdown-nested-example.css']
+})
+export class DropdownNestedExample {}
