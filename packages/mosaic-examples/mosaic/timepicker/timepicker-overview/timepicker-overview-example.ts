aec16f51101c8aa9b0af517bda7f6967d580330b
myakovleva <myakovleva@ptsecurity.com>
2019-10-30 15:17:32 +0300

    feat(docs): adding alerts example

---
+++ packages/mosaic-examples/mosaic/timepicker/timepicker-overview/timepicker-overview-example.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Timepicker
+ */
+@Component({
+    selector: 'timepicker-overview-example',
+    templateUrl: 'timepicker-overview-example.html',
+    styleUrls: ['timepicker-overview-example.css']
+})
+export class TimepickerOverviewExample {
+}
+
+
