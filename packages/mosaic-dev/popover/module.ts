0ad1adbd58d25a87dcb6eda43b02d27952b17886
lskramarov <lskramarov@users.noreply.github.com>
2019-11-14 18:37:51 +0300

    fix(popover): added trigger click and it was set by default (#UIM-238) (#338)

--- packages/mosaic-dev/popover/module.ts
+++ packages/mosaic-dev/popover/module.ts
@@ -16,20 +16,9 @@ import { McIconModule } from '../../mosaic/icon/';
     template: require('./template.html')
 })
 export class DemoComponent {
-    private popoverActiveStage: number;
+    popoverActiveStage: number;
 
-    private isPopoverVisibleLeft: boolean = false;
-    private isPopoverVisibleLeftTop: boolean = false;
-    private isPopoverVisibleLeftBottom: boolean = false;
-    private isPopoverVisibleBottom: boolean = false;
-    private isPopoverVisibleBottomRight: boolean = false;
-    private isPopoverVisibleBottomLeft: boolean = false;
-    private isPopoverVisibleRight: boolean = false;
-    private isPopoverVisibleRightTop: boolean = false;
-    private isPopoverVisibleRightBottom: boolean = false;
-    private isPopoverVisibleTop: boolean = false;
-    private isPopoverVisibleTopRight: boolean = false;
-    private isPopoverVisibleTopLeft: boolean = false;
+    isPopoverVisibleLeft: boolean = false;
 
     constructor() {
         this.popoverActiveStage = 1;
@@ -43,96 +32,8 @@ export class DemoComponent {
         this.isPopoverVisibleLeft = !this.isPopoverVisibleLeft;
     }
 
-    changePopoverVisibilityLeftTop() {
-        this.isPopoverVisibleLeftTop = !this.isPopoverVisibleLeftTop;
-    }
-
-    changePopoverVisibilityLeftBottom() {
-        this.isPopoverVisibleLeftBottom = !this.isPopoverVisibleLeftBottom;
-    }
-
-    changePopoverVisibilityBottom() {
-        this.isPopoverVisibleBottom = !this.isPopoverVisibleBottom;
-    }
-
-    changePopoverVisibilityBottomRight() {
-        this.isPopoverVisibleBottomRight = !this.isPopoverVisibleBottomRight;
-    }
-
-    changePopoverVisibilityBottomLeft() {
-        this.isPopoverVisibleBottomLeft = !this.isPopoverVisibleBottomLeft;
-    }
-
-    changePopoverVisibilityRight() {
-        this.isPopoverVisibleRight = !this.isPopoverVisibleRight;
-    }
-
-    changePopoverVisibilityRightTop() {
-        this.isPopoverVisibleRightTop = !this.isPopoverVisibleRightTop;
-    }
-
-    changePopoverVisibilityRightBottom() {
-        this.isPopoverVisibleRightBottom = !this.isPopoverVisibleRightBottom;
-    }
-
-    changePopoverVisibilityTop() {
-        this.isPopoverVisibleTop = !this.isPopoverVisibleTop;
-    }
-
-    changePopoverVisibilityTopRight() {
-        this.isPopoverVisibleTopRight = !this.isPopoverVisibleTopRight;
-    }
-
-    changePopoverVisibilityTopLeft() {
-        this.isPopoverVisibleTopLeft = !this.isPopoverVisibleTopLeft;
-    }
-
     onPopoverVisibleChangeLeft(update: boolean) {
         this.isPopoverVisibleLeft = update;
-    }
-
-    onPopoverVisibleChangeLeftTop(update: boolean) {
-        this.isPopoverVisibleLeftTop = update;
-    }
-
-    onPopoverVisibleChangeLeftBottom(update: boolean) {
-        this.isPopoverVisibleLeftBottom = update;
-    }
-
-    onPopoverVisibleChangeBottom(update: boolean) {
-        this.isPopoverVisibleBottom = update;
-    }
-
-    onPopoverVisibleChangeBottomRight(update: boolean) {
-        this.isPopoverVisibleBottomRight = update;
-    }
-
-    onPopoverVisibleChangeBottomLeft(update: boolean) {
-        this.isPopoverVisibleBottomLeft = update;
-    }
-
-    onPopoverVisibleChangeRight(update: boolean) {
-        this.isPopoverVisibleRight = update;
-    }
-
-    onPopoverVisibleChangeRightTop(update: boolean) {
-        this.isPopoverVisibleRightTop = update;
-    }
-
-    onPopoverVisibleChangeRightBottom(update: boolean) {
-        this.isPopoverVisibleRightBottom = update;
-    }
-
-    onPopoverVisibleChangeTop(update: boolean) {
-        this.isPopoverVisibleTop = update;
-    }
-
-    onPopoverVisibleChangeTopRight(update: boolean) {
-        this.isPopoverVisibleTopRight = update;
-    }
-
-    onPopoverVisibleChangeTopLeft(update: boolean) {
-        this.isPopoverVisibleTopLeft = update;
     }
 }
 
