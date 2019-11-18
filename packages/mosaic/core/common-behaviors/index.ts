73539a6d96ab857c965c9e1c8f42f1a30c9bad13
lkramarov <lkramarov@ptsecurity.com>
2019-11-18 16:40:42 +0300

    MultipleMode moved in selection/constants

--- packages/mosaic/core/common-behaviors/index.ts
+++ packages/mosaic/core/common-behaviors/index.ts
@@ -4,9 +4,3 @@ export { CanDisable, CanDisableCtor, mixinDisabled } from './disabled';
 export { CanColor, CanColorCtor, mixinColor, ThemePalette } from './color';
 export { HasTabIndex, HasTabIndexCtor, mixinTabIndex } from './tabindex';
 export { CanUpdateErrorStateCtor, CanUpdateErrorState, mixinErrorState } from './error-state';
-
-export enum MultipleMode {
-    CHECKBOX = 'checkbox',
-    KEYBOARD = 'keyboard'
-}
-
