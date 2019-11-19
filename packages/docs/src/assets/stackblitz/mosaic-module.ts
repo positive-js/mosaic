e750b92758f109c7930e1ac383b7c9ff757ed0f3
Iakovleva Margarita <naruyaoi@yandex.ru>
2019-11-19 12:55:14 +0300

    docs: add datepicker examples (#344)

--- packages/docs/src/assets/stackblitz/mosaic-module.ts
+++ packages/docs/src/assets/stackblitz/mosaic-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { McMomentDateModule } from '@ptsecurity/mosaic-moment-adapter';
 import { McButtonModule } from '@ptsecurity/mosaic/button';
 import { McCheckboxModule } from '@ptsecurity/mosaic/checkbox';
+import { McDatepickerModule } from '@ptsecurity/mosaic/datepicker';
 import { McFormFieldModule } from '@ptsecurity/mosaic/form-field';
 import { McIconModule } from '@ptsecurity/mosaic/icon';
 import { McInputModule } from '@ptsecurity/mosaic/input';
@@ -14,6 +15,7 @@ import { McSelectModule } from '@ptsecurity/mosaic/select';
         McMomentDateModule,
         McButtonModule,
         McCheckboxModule,
+        McDatepickerModule,
         McFormFieldModule,
         McIconModule,
         McInputModule,
