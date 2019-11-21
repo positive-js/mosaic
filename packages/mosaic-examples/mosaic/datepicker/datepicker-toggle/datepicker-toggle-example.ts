e0f404ca09181ecaa1343aa146d43026e6e94be4
Iakovleva Margarita <naruyaoi@yandex.ru>
2019-11-21 21:47:45 +0300

    feat(docs): adding datapicker examples (#347)
    datepicker-language, datepicker-toggle and datepicker-year examples are added

---
+++ packages/mosaic-examples/mosaic/datepicker/datepicker-toggle/datepicker-toggle-example.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Datepicker with custom toggle
+ */
+
+@Component({
+    selector: 'datepicker-toggle-example',
+    templateUrl: 'datepicker-toggle-example.html',
+    styleUrls: ['datepicker-toggle-example.css']
+})
+export class DatepickerToggleExample {
+    date: Date;
+}
