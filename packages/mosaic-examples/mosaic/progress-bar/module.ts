3a11bec97068a8706117e00cddb6818121ee23e1
myakovleva <myakovleva@ptsecurity.com>
2019-11-26 18:59:24 +0300

    feat(docs): adding progress bar examples

---
+++ packages/mosaic-examples/mosaic/progress-bar/module.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { McFormFieldModule } from '@ptsecurity/mosaic/form-field';
+import { McInputModule } from '@ptsecurity/mosaic/input';
+import { McProgressBarModule } from '@ptsecurity/mosaic/progress-bar';
+
+import { ProgressBarIndeterminateExample } from './progress-bar-indeterminate/progress-bar-indeterminate-example';
+import { ProgressBarOverviewExample } from './progress-bar-overview/progress-bar-overview-example';
+
+
+const EXAMPLES = [
+    ProgressBarIndeterminateExample,
+    ProgressBarOverviewExample
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        McFormFieldModule,
+        McInputModule,
+        McProgressBarModule
+    ],
+    declarations: EXAMPLES,
+    exports: EXAMPLES
+})
+export class ProgressBarExamplesModule {
+}
