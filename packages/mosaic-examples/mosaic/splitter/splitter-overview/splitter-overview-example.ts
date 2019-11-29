71fd5d8e50f09435a9d6308f5eb2b4dccdbb02d3
lkramarov <lkramarov@ptsecurity.com>
2019-11-29 16:52:56 +0300

    feat(docs): added examples for card and splitter (#UIM-290)

---
+++ packages/mosaic-examples/mosaic/splitter/splitter-overview/splitter-overview-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Basic Splitter
+ */
+@Component({
+    selector: 'splitter-overview-example',
+    templateUrl: 'splitter-overview-example.html',
+    styleUrls: ['splitter-overview-example.css']
+})
+export class SplitterOverviewExample {}
---
+++ packages/mosaic-examples/mosaic/splitter/splitter-vertical/splitter-vertical-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Basic Splitter
+ */
+@Component({
+    selector: 'splitter-vertical-example',
+    templateUrl: 'splitter-vertical-example.html',
+    styleUrls: ['splitter-vertical-example.css']
+})
+export class SplitterVerticalExample {}
