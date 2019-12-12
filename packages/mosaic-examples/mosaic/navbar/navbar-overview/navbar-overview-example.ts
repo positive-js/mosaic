88d1da7fe799ff9aa6dd524011c8f8cd8f90efec
lskramarov <lskramarov@users.noreply.github.com>
2019-12-12 18:50:29 +0300

    feat(docs): added navbar and tabs (#UIM-326) (#366)

---
+++ packages/mosaic-examples/mosaic/navbar/navbar-overview/navbar-overview-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Navbar
+ */
+@Component({
+    selector: 'navbar-overview-example',
+    templateUrl: 'navbar-overview-example.html',
+    styleUrls: ['navbar-overview-example.css']
+})
+export class NavbarOverviewExample {}
---
+++ packages/mosaic-examples/mosaic/tabs/tabs-overview/tabs-overview-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Navbar
+ */
+@Component({
+    selector: 'tabs-overview-example',
+    templateUrl: 'tabs-overview-example.html',
+    styleUrls: ['tabs-overview-example.css']
+})
+export class TabsOverviewExample {}
