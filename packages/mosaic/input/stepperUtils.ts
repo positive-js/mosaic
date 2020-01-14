83bbe6890c22d2481d2fcf7d9094890ebcd2fc0c
lkramarov <lkramarov@ptsecurity.com>
2020-01-14 16:58:22 +0300

    fix(number): stepUp and stepDown are not work with empty value (#UIM-333)

--- packages/mosaic/input/stepperUtils.ts
+++ packages/mosaic/input/stepperUtils.ts
@@ -1,9 +1,3 @@
-function sanitizeNumber(value: number): number | null {
-    return !isFinite(value) || isNaN(value)
-        ? null
-        : value;
-}
-
 function getPrecision(value: number): number {
     const arr = value.toString().split('.');
 
@@ -16,43 +10,13 @@ function getPrecision(value: number): number {
 function add(value1: number, value2: number) {
     const precision = Math.max(getPrecision(value1), getPrecision(value2));
 
-    const res = (value1 * precision + value2 * precision) / precision;
-
-    return sanitizeNumber(res);
+    return (value1 * precision + value2 * precision) / precision;
 }
 
-export const stepUp = (value: number | null,
-                       max: number,
-                       min: number,
-                       step: number
-): number | null => {
-    let res;
-
-    if (value === null) {
-        res = add(min, step);
-
-        return res === null ? null : Math.min(res, max);
-    }
-
-    res = add(value, step);
-
-    return res === null ? null : Math.max(Math.min(res, max), min);
+export const stepUp = (value: number, max: number, min: number, step: number): number => {
+    return Math.max(Math.min(add(value, step), max), min);
 };
 
-export const stepDown = (value: number | null,
-                         max: number,
-                         min: number,
-                         step: number
-): number | null => {
-    let res;
-
-    if (value === null) {
-        res = add(max, -step);
-
-        return res === null ? null : Math.max(res, min);
-    }
-
-    res = add(value, -step);
-
-    return res === null ? null : Math.min(Math.max(res, min), max);
+export const stepDown = (value: number, max: number, min: number, step: number): number => {
+    return Math.min(Math.max(add(value, -step), min), max);
 };
