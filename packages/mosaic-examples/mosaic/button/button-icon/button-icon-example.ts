065a14e9ef5c9f32e3940720dc2e4e13eba4af7f
myakovleva <myakovleva@ptsecurity.com>
2020-01-15 18:52:36 +0300

    feat(docs): adding button examples

---
+++ packages/mosaic-examples/mosaic/button/button-icon/button-icon-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Icon button
+ */
+@Component({
+    selector: 'button-icon-example',
+    templateUrl: 'button-icon-example.html',
+    styleUrls: ['button-icon-example.css']
+})
+export class ButtonIconExample {}
