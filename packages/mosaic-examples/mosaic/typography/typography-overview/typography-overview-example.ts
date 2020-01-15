081bb22d22da8bc57ee4b681afabecf3f635830e
Iakovleva Margarita <naruyaoi@yandex.ru>
2020-01-16 01:23:50 +0300

    feat(docs): adding typography example (#386)

---
+++ packages/mosaic-examples/mosaic/typography/typography-overview/typography-overview-example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Basic typography
+ */
+@Component({
+    selector: 'typography-overview-example',
+    templateUrl: 'typography-overview-example.html',
+    styleUrls: ['typography-overview-example.css']
+})
+export class TypographyOverviewExample {}
--- packages/mosaic/core/styles/typography/typography.md
+++ packages/mosaic/core/styles/typography/typography.md
@@ -1,3 +1,5 @@
+<!-- example(typography-overview) -->
+
 ### Usage and Override
 
 ```scss
