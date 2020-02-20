b4b3e9a4965f75707ebf660c1c938d3dc561058b
lskramarov <lskramarov@users.noreply.github.com>
2020-02-20 18:50:31 +0300

    fix(tags): incorrect validation (UIM-367) (#416)
    8x

--- packages/mosaic/tags/tag-list.component.ts
+++ packages/mosaic/tags/tag-list.component.ts
@@ -823,6 +823,8 @@ export class McTagList extends McTagListMixinBase implements McFormFieldControl<
             // receive focus.
             if (this.isValidIndex(tagIndex) && tag.hasFocus) {
                 this.lastDestroyedTagIndex = tagIndex;
+            } else if (this.isValidIndex(tagIndex) && !tag.hasFocus) {
+                this.focusInput();
             }
         });
     }
@@ -832,9 +834,7 @@ export class McTagList extends McTagListMixinBase implements McFormFieldControl<
         let currentElement = event.target as HTMLElement | null;
 
         while (currentElement && currentElement !== this.elementRef.nativeElement) {
-            if (currentElement.classList.contains('mc-tag')) {
-                return true;
-            }
+            if (currentElement.classList.contains('mc-tag')) { return true; }
 
             currentElement = currentElement.parentElement;
         }
