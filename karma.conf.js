c3467c34833ae2c58d9f7ec297e8a547b161e860
Oleg Pimenov <pimenov.o@gmail.com>
2020-03-11 16:03:41 +0300

    build: updated circleci flow

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = () => {
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
-            require('karma-coverage'),
+            require('karma-coverage-istanbul-reporter'),
             require('karma-junit-reporter'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
@@ -17,18 +17,17 @@ module.exports = () => {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
 
-        reporters: ['dots'],
+        reporters: ['dots', 'junit'],
+
         port: 9876,
         colors: true,
         logLevel: 'INFO',
         autoWatch: false,
 
-        coverageReporter: {
-            dir : 'dist/coverage/',
-            subdir: '.',
-            reporters: [
-                { type: 'lcov' }
-            ]
+        coverageIstanbulReporter: {
+            dir: 'dist/coverage/',
+            reports: ['html', 'lcovonly', 'text-summary'],
+            fixWebpackSourcePaths: true
         },
 
         junitReporter: {
