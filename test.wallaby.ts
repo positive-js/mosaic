259eb952a969541a92485bf60e75b1575505ba06
Oleg Pimenov <pimenov.o@gmail.com>
2020-04-03 16:05:59 +0300

    build: updated wallaby config and some fix for global sass imports

---
+++ test.wallaby.ts
@@ -0,0 +1,13 @@
+// tslint:disable
+import 'zone.js/dist/zone-testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+
+getTestBed().initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
+);
