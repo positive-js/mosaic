eaeeb5a79e3ff3c9c47e9a39a7795a00f10980f9
Oleg Pimenov <pimenov.o@gmail.com>
2020-04-06 22:38:20 +0300

    chore: added dev-all example

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,10 @@ module.exports = () => {
             require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
+            clearContext: false, // leave Jasmine Spec Runner output visible in browser
+            jasmine: {
+                random: false
+            }
         },
         angularCli: {
             sourceMap: true
