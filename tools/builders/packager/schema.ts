db7c70ce3c0bbef1c3a85e67e0af7603209e5fe6
Oleg Pimenov <pimenov.o@gmail.com>
2020-04-08 23:51:49 +0300

    chore: updated schema for builder

--- tools/builders/packager/schema.ts
+++ tools/builders/packager/schema.ts
@@ -1,14 +1,5 @@
-export interface IPackagerAssetDef {
-    glob: string;
-    input: string;
-    output: string;
-}
-
 export interface IPackagerOptions {
     buildTarget: string;
-    releaseCDKPackageJson: string;
-    releaseMosaicPackageJson: string;
     versionPlaceholder: string;
     ngVersionPlaceholder: string;
-    styles: IPackagerAssetDef[];
 }
