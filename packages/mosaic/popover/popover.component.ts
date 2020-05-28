844ee1cf21a865a56e1a231773f9bcb4f3a51066
LeonVay <leon.vay@gmail.com>
2020-05-28 11:03:23 +0300

    fix(popover): Update on resize handler remove

--- packages/mosaic/popover/popover.component.ts
+++ packages/mosaic/popover/popover.component.ts
@@ -533,7 +533,6 @@ export class McPopover implements OnInit, OnDestroy {
     }
 
     onPositionChange($event: ConnectedOverlayPositionChange): void {
-        console.log('position change');
         let updatedPlacement = this.mcPlacement;
         Object.keys(this.availablePositions).some((key) => {
             if ($event.connectionPair.originX === this.availablePositions[key].originX &&
