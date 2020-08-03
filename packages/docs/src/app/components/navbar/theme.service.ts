474dbe9c024374dd88a1fa970db4fb8efcaf95b8
KuryakovDen <fifer2@yandex.ru>
2020-08-03 13:50:33 +0700

    feat(navbar): maked automatic switching in the choice of the theme (#UIM-463) (#482)
    Co-authored-by: Kuryakov Denis <KurjakovDenis@yandex.ru>

---
+++ packages/docs/src/app/components/navbar/theme.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, ApplicationRef } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+
+export enum Themes {
+  Default = 'theme-default',
+  Dark = 'theme-dark'
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ThemeService {
+  currentTheme = new BehaviorSubject(Themes.Default);
+
+  constructor(private ref: ApplicationRef) {
+    const isLightTheme: boolean = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+
+    isLightTheme ? this.currentTheme.next(Themes.Default) : this.currentTheme.next(Themes.Dark);
+
+    window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', (e) => {
+      const turnOn = e.matches;
+      this.currentTheme.next(turnOn ? Themes.Default : Themes.Dark);
+
+      this.ref.tick();
+    });
+  }
+
+  setTheme(value) {
+    this.currentTheme.next(value);
+  }
+
+  getTheme() {
+    return this.currentTheme.getValue();
+  }
+}
