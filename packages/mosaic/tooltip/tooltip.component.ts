c00900e75576bab08e0c3e4c6c34e13c34d698d4
Andrew Vlasov <13451179+dron1ch@users.noreply.github.com>
2020-10-30 15:13:10 +0300

    fix: updated positioning logic in tooltips (#UIM-511) (#514)

--- packages/mosaic/tooltip/tooltip.component.ts
+++ packages/mosaic/tooltip/tooltip.component.ts
@@ -44,6 +44,14 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 
+export enum ArrowPlacements {
+    Top = 'top',
+    Center = 'center',
+    Bottom = 'bottom',
+    Right = 'right',
+    Left = 'left'
+}
+
 @Component({
     selector: 'mc-tooltip-component',
     animations: [fadeAnimation],
@@ -111,6 +119,17 @@ export class McTooltipComponent {
     private _mcPlacement: string = 'top';
 
     @Input()
+    get mcTooltipClass(): string {
+        return this._mcTooltipClass;
+    }
+
+    set mcTooltipClass(value: string) {
+        this._mcTooltipClass = value;
+    }
+
+    private _mcTooltipClass: string;
+
+    @Input()
     get mcVisible(): boolean {
         return this._mcVisible.value;
     }
@@ -126,6 +145,17 @@ export class McTooltipComponent {
     }
 
     private _mcVisible: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+    @Input()
+    get mcArrowPlacement(): ArrowPlacements {
+        return this._mcArrowPlacement;
+    }
+
+    set mcArrowPlacement(value: ArrowPlacements) {
+        this._mcArrowPlacement = value;
+    }
+
+    private _mcArrowPlacement: ArrowPlacements;
 
     /** Subject for notifying that the tooltip has been hidden from the view */
     private readonly onHideSubject: Subject<any> = new Subject();
@@ -370,6 +400,17 @@ export class McTooltip implements OnInit, OnDestroy {
 
     private _mcVisible: boolean;
 
+    @Input('mcArrowPlacement')
+    get mcArrowPlacement(): ArrowPlacements {
+        return this._mcArrowPlacement;
+    }
+
+    set mcArrowPlacement(value: ArrowPlacements) {
+        this._mcArrowPlacement = value;
+    }
+
+    private _mcArrowPlacement: ArrowPlacements;
+
     @HostBinding('class.mc-tooltip-open')
     get isOpen(): boolean {
         return this.isTooltipOpen;
@@ -482,13 +523,29 @@ export class McTooltip implements OnInit, OnDestroy {
         }
 
         if (this.mcPlacement === 'right' || this.mcPlacement === 'left') {
-            const pos =
-                (this.overlayRef.overlayElement.clientHeight -
-                    this.hostView.element.nativeElement.clientHeight) / 2; // tslint:disable-line
-            const currentContainer = this.overlayRef.overlayElement.style.top || '0px';
-            this.overlayRef.overlayElement.style.top =
-                `${parseInt(currentContainer.split('px')[0], 10) + pos - 1}px`;
-            // TODO: обновлять положение стрелки\указателя\"дятла"
+            const halfDelimeter = 2;
+            const overlayElemHeight = this.overlayRef.overlayElement.clientHeight;
+            const currentContainerHeight = this.hostView.element.nativeElement.clientHeight;
+
+            if (this.mcArrowPlacement === ArrowPlacements.Center) {
+                const arrowElemRef = this.getTooltipArrowElem();
+                const currentContainerPositionTop = parseInt(this.hostView.element.nativeElement.offsetTop, 10);
+                const currentContainerHeightHalfed = currentContainerHeight / halfDelimeter;
+                const tooltipHeightHalfed = overlayElemHeight / halfDelimeter;
+
+                this.overlayRef.overlayElement.style.top = `${
+                    (currentContainerPositionTop + currentContainerHeightHalfed) - tooltipHeightHalfed + 1
+                }px`;
+
+                if (arrowElemRef) {
+                    arrowElemRef.setAttribute('style', `top: ${tooltipHeightHalfed - 1}px`);
+                }
+            } else {
+                const pos = (overlayElemHeight - currentContainerHeight) / halfDelimeter;
+                const defaultTooltipPlacementTop = parseInt(this.overlayRef.overlayElement.style.top || '0px', 10);
+
+                this.overlayRef.overlayElement.style.top = `${defaultTooltipPlacementTop + pos - 1}px`;
+            }
         }
     }
 
@@ -533,7 +590,7 @@ export class McTooltip implements OnInit, OnDestroy {
         if (this.mcTrigger === 'hover') {
             this.manualListeners
                 .set('mouseenter', () => this.show())
-                .set('mouseleave', () => this.hide())
+                // .set('mouseleave', () => this.hide())
                 .forEach((listener, event) => this.elementRef.nativeElement.addEventListener(event, listener));
         }
 
@@ -691,5 +748,11 @@ export class McTooltip implements OnInit, OnDestroy {
         }
 
         return { x: newX, y: newY };
+    }
+
+    private getTooltipArrowElem() {
+        const arrowClassName = 'mc-tooltip-arrow';
+
+        return this.overlayRef?.overlayElement.getElementsByClassName(arrowClassName)[0];
     }
 }
