6d154220983bb3e3106cc00a2f92998d1da8e6a8
l.s.kramarov <l.s.kramarov@gmail.com>
2020-12-02 18:05:16 +0300

    added one more example for composite form

--- packages/mosaic-examples/mosaic/validation/validation-composite/validation-composite-example.ts
+++ packages/mosaic-examples/mosaic/validation/validation-composite/validation-composite-example.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 
@@ -32,12 +32,51 @@ const IP_PATTERN = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-
     encapsulation: ViewEncapsulation.None
 })
 export class ValidationCompositeExample {
-    compositeForm: FormGroup;
+    compositeFormWithOnTypeChecking: FormGroup;
+    compositeFormWithOnBlurChecking: FormGroup;
+
+    @ViewChild('startTooltip', { static: false }) startTooltip: any;
+    @ViewChild('endTooltip', { static: false }) endTooltip: any;
 
     constructor() {
-        this.compositeForm = new FormGroup({
+        this.compositeFormWithOnTypeChecking = new FormGroup({
             start: new FormControl('', [Validators.pattern(IP_PATTERN)]),
             end: new FormControl('', [Validators.pattern(IP_PATTERN)])
         }, compositeFormValidator());
+
+        this.compositeFormWithOnBlurChecking = new FormGroup({
+            start: new FormControl('', [Validators.pattern(IP_PATTERN)]),
+            end: new FormControl('', [Validators.pattern(IP_PATTERN)])
+        }, compositeFormValidator());
+    }
+
+    onInputStart(event) {
+        const regex = /^[\d\.]+$/g;
+
+        if (!regex.test(event.target.value)) {
+            event.target.value = event.target.value.replace(/\D+/g, '');
+
+            if (!this.startTooltip.isTooltipOpen) {
+                this.startTooltip.show();
+
+                // tslint:disable-next-line:no-magic-numbers
+                setTimeout(() => this.startTooltip.hide(), 3000);
+            }
+        }
+    }
+
+    onInputEnd(event) {
+        const regex = /^[\d\.]+$/g;
+
+        if (!regex.test(event.target.value)) {
+            event.target.value = event.target.value.replace(/\D+/g, '');
+
+            if (!this.endTooltip.isTooltipOpen) {
+                this.endTooltip.show();
+
+                // tslint:disable-next-line:no-magic-numbers
+                setTimeout(() => this.endTooltip.hide(), 3000);
+            }
+        }
     }
 }
