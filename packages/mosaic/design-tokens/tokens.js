92246f7a7b21aa7e587afe07ed7fd1b2758299ed
l.s.kramarov <l.s.kramarov@gmail.com>
2020-12-02 16:15:43 +0300

    added style-dictionary

---
+++ packages/mosaic/design-tokens/tokens.js
@@ -0,0 +1,34 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 02 Dec 2020 12:47:30 GMT
+ */
+
+export const ColorBackgroundPrimary = "#ffffff";
+export const ColorBackgroundLink = "#0366d6";
+export const ColorBackgroundInverse = "#111111";
+export const ColorBaseGrayLight = "#cccccc";
+export const ColorBaseGrayMedium = "#999999";
+export const ColorBaseGrayDark = "#111111";
+export const ColorBaseRed = "#ff0000";
+export const ColorBaseGreen = "#00ff00";
+export const ColorCoreWhite = "#ffffff";
+export const ColorCoreBlack = "#111111";
+export const ColorCoreGray = "#dddddd";
+export const ColorCoreBlue = "#0366d6";
+export const ColorFontPrimary = "#111111";
+export const ColorFontLink = "#0366d6";
+export const ColorFontInverse = "#ffffff";
+export const SizeFontSmall = "0.75rem"; // the small size of the font
+export const SizeFontMedium = "1rem"; // the medium size of the font
+export const SizeFontLarge = "2rem"; // the large size of the font
+export const SizeFontBase = "1rem"; // the base size of the font
+export const SizePaddingSmall = "0.5rem";
+export const SizePaddingMedium = "1rem";
+export const SizePaddingLarge = "2rem";
+export const ComponentButtonPadding = "1rem";
+export const ComponentButtonFontSize = 2;
+export const ComponentButtonTextAlign = "center";
+export const ComponentButtonPrimaryBackgroundColor = "hsl(10, 80, 50)";
+export const ComponentButtonPrimaryColor = "#ffffff";
+export const ComponentButtonSecondaryBackgroundColor = "#ffffff";
+export const ComponentButtonSecondaryColor = "#0366d6";
