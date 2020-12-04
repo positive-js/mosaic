9efc3402e35ddc64f3333bc0fac84c2c35d11f18
l.s.kramarov <l.s.kramarov@gmail.com>
2020-12-04 17:25:17 +0300

    feat(docs): added examples for popover (#UIM-545)

--- packages/mosaic-examples/mosaic/popover/module.ts
+++ packages/mosaic-examples/mosaic/popover/module.ts
@@ -1,22 +1,40 @@
 import { A11yModule } from '@angular/cdk/a11y';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { McButtonModule } from '@ptsecurity/mosaic/button';
+import { McCheckboxModule } from '@ptsecurity/mosaic/checkbox';
+import { McFormFieldModule } from '@ptsecurity/mosaic/form-field';
+import { McIconModule } from '@ptsecurity/mosaic/icon';
+import { McInputModule } from '@ptsecurity/mosaic/input';
 import { McPopoverModule } from '@ptsecurity/mosaic/popover';
+import { McSelectModule } from '@ptsecurity/mosaic/select';
+import { McSplitterModule } from '@ptsecurity/mosaic/splitter';
 
+import { PopoverInstanceExample } from './popover-instance/popover-instance-example';
 import { PopoverOverviewExample } from './popover-overview/popover-overview-example';
 
 
 const EXAMPLES = [
-    PopoverOverviewExample
+    PopoverOverviewExample,
+    PopoverInstanceExample
 ];
 
 @NgModule({
     imports: [
+        BrowserModule,
         BrowserAnimationsModule,
         A11yModule,
+        FormsModule,
+        McFormFieldModule,
+        McSelectModule,
+        McPopoverModule,
         McButtonModule,
-        McPopoverModule
+        McIconModule,
+        McInputModule,
+        McSplitterModule,
+        McCheckboxModule
     ],
     declarations: EXAMPLES,
     exports: EXAMPLES
