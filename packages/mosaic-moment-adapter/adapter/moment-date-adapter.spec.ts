f97983cf3fa8f8728f1ffdfdbc0945cbaece208c
Anna Manchenkova <37004408+anywie@users.noreply.github.com>
2021-02-17 09:20:33 +0300

    fix(formatter): Date-formatter comma (#UIM-454) (#571)
    * fix(formatter): Date-formatter comma (#UIM-454)
    
    * fix(formatter): test_unit_mosaic-moment-adapter (#UIM-454)
    
    Co-authored-by: amanchenkova <amanchenkova@ptsecurity.com>

--- packages/mosaic-moment-adapter/adapter/moment-date-adapter.spec.ts
+++ packages/mosaic-moment-adapter/adapter/moment-date-adapter.spec.ts
@@ -659,7 +659,7 @@ describe('MomentDateAdapter formatter', () => {
                 it('before yesterday (other year)', () => {
                     const date = moment().year(2015).subtract(49, 'hours');
                     expect(adapter.relativeShortDate(date)).toBe(
-                        date.format(`${DAY}${NBSP}${SHORT_MONTH}, ${YEAR}`)
+                        date.format(`${DAY}${NBSP}${SHORT_MONTH} ${YEAR}`)
                     );
                 });
             });
@@ -706,7 +706,7 @@ describe('MomentDateAdapter formatter', () => {
 
                 it('before yesterday (other year)', () => {
                     const date = moment().year(2015).subtract(49, 'hours');
-                    expect(adapter.relativeLongDate(date)).toBe(date.format(`${DAY_MONTH}, ${YEAR}`));
+                    expect(adapter.relativeLongDate(date)).toBe(date.format(`${DAY_MONTH} ${YEAR}`));
                 });
             });
         });
