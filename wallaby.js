7608cf49ca70abaf0c129282621f5712ab4171dd
l.s.kramarov <l.s.kramarov@gmail.com>
2021-03-05 11:45:19 +0300

    added files in wallaby.js for hide message "No files with tests found, check your tests list patterns."

--- wallaby.js
+++ wallaby.js
@@ -1,6 +1,10 @@
 
 module.exports = () => ({
     autoDetect: true,
+    files: [
+        'packages/**/*.ts',
+        "!packages/**/*.spec.ts",
+    ],
     tests: [
         "packages/**/*.spec.ts",
         "!packages/mosaic/schematics/**/*.spec.ts",
