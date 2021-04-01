b56249fd9fa7bae5301a46516b7dc28055872285
lskramarov <lskramarov@users.noreply.github.com>
2021-04-01 10:46:56 +0300

    fix(typography): need generate _typography.scss without '!default' (UIM-611) (#596)
    * fix(typography): need _typography.scss without !default (UIM-611)
    
    * fixed code smell

--- packages/mosaic/design-tokens/build.js
+++ packages/mosaic/design-tokens/build.js
@@ -15,6 +15,26 @@ function getMapFromObj(object) {
     return `(\n${result}\n)`
 }
 
+function processJsonNode(obj, depth) {
+    let output = '';
+
+    if (obj.hasOwnProperty('value')) {
+        output += `$${obj.name}`;
+    } else {
+        output += '(\n'
+        output += Object.keys(obj).map((newKey) => {
+            const newProp = obj[newKey];
+            const indent = '  '.repeat(depth+1);
+
+            return `${indent}'${newKey}': ${processJsonNode(newProp, depth + 1)}`;
+        }).join(',\n');
+
+        output += '\n' + '  '.repeat(depth) + ')';
+    }
+
+    return output;
+}
+
 console.log('Build started...');
 console.log('\n==============================================');
 
@@ -57,6 +77,35 @@ StyleDictionary.registerFormat({
         return dictionary.allProperties
             .map((prop) => `\$${prop.name}: ${getMapFromObj(prop.value)};\n`)
             .join('\n');
+    }
+})
+
+StyleDictionary.registerFormat({
+    name: 'mc-scss/typography',
+    formatter: function(dictionary) {
+        let string;
+
+        string = dictionary.allProperties
+            .map((prop) => {
+                let output = '';
+
+                const value = prop.attributes.category === 'asset' ? `"${ prop.value }"` : prop.value;
+
+                output += `$${ prop.name }: ${ value };`
+
+                if (prop.comment) {
+                    output += ` // ${ prop.comment }`;
+                }
+
+                output += '\n';
+
+                return output;
+            })
+            .join('');
+
+        string += `\n$${this.mapName || 'tokens' }: ${processJsonNode(dictionary.properties, 0)};\n`;
+
+        return string;
     }
 })
 
