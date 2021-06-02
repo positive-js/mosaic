7157e84880c129ca0821972e7e4eb9f5bcc61332
l.s.kramarov <l.s.kramarov@gmail.com>
2021-06-02 11:35:59 +0300

    fixed build errors

--- packages/mosaic/modal/public-api.ts
+++ packages/mosaic/modal/public-api.ts
@@ -2,4 +2,5 @@ export { McModalComponent } from './modal.component';
 export { McModalRef } from './modal-ref.class';
 export { McModalModule } from './modal.module';
 export { McModalService } from './modal.service';
+export * from './modal.directive';
 export * from './modal.type';
