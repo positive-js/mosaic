71ee27d56e4b0c271d4a43e2760917457a35345e
Oleg Pimenov <pimenov.o@gmail.com>
2021-07-21 15:17:36 +0300

    feat: added common build script for tokens and added tokens for docs

--- packages/mosaic/design-tokens/build-tokens.js
+++ packages/mosaic/design-tokens/build-tokens.js
@@ -1,11 +1,11 @@
 require('./style-dictionary/build')([
-    // {
-    //     name: 'default-theme',
-    //     buildPath: [
-    //         `packages/mosaic/design-tokens/tokens/properties/**/*.json5`,
-    //         `packages/mosaic/design-tokens/tokens/components/**/*.json5`
-    //     ],
-    //     outputPath: 'packages/mosaic/design-tokens/'
-    // }
+    {
+        name: 'default-theme',
+        buildPath: [
+            `packages/mosaic/design-tokens/tokens/properties/**/*.json5`,
+            `packages/mosaic/design-tokens/tokens/components/**/*.json5`
+        ],
+        outputPath: 'packages/mosaic/design-tokens/'
+    }
 ]);
 
