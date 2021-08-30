5370348392c600df2448506ccb211bd5fc49ce67
l.s.kramarov <l.s.kramarov@gmail.com>
2021-08-30 16:52:39 +0300

    fix(navbar): correct loading design-tokens (#UIM-737)

--- packages/mosaic/navbar/vertical-navbar.animation.ts
+++ packages/mosaic/navbar/vertical-navbar.animation.ts
@@ -1,10 +1,14 @@
 import { animate, AnimationTriggerMetadata, style, transition, trigger, state } from '@angular/animations';
+import {
+    VerticalNavbarSizeStatesClosedWidth as closedWidth,
+    VerticalNavbarSizeStatesOpenedWidth as openedWidth
+} from '@ptsecurity/mosaic/design-tokens';
 
 
 export function toggleVerticalNavbarAnimation(): AnimationTriggerMetadata {
     return trigger('toggle', [
-        state('0', style({ width: '48px' })),
-        state('1',  style({ width: '240px' })),
+        state('0', style({ width: `var(--mc-vertical-navbar-size-states-closed-width, ${closedWidth})` })),
+        state('1',  style({ width: `var(--mc-vertical-navbar-size-states-opened-width, ${openedWidth})` })),
         transition('0 <=> 1', animate('200ms cubic-bezier(0, 1, 0.5, 1)'))
     ]);
 }
