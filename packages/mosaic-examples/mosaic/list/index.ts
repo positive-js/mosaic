48a1383a139feed7a5552634146a4af83ed15f01
l.s.kramarov <l.s.kramarov@gmail.com>
2021-10-29 20:41:36 +0300

    added examples in docs

--- packages/mosaic-examples/mosaic/list/index.ts
+++ packages/mosaic-examples/mosaic/list/index.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { McDropdownModule } from '@ptsecurity/mosaic/dropdown';
+import { McIconModule } from '@ptsecurity/mosaic/icon';
 import { McListModule } from '@ptsecurity/mosaic/list';
+import { McToolTipModule } from '@ptsecurity/mosaic/tooltip';
 
+import { ListActionButtonExample } from './list-action-button/list-action-button-example';
 import { ListGroupsExample } from './list-groups/list-groups-example';
 import { ListMultipleCheckboxExample } from './list-multiple-checkbox/list-multiple-checkbox-example';
 import { ListMultipleKeyboardExample } from './list-multiple-keyboard/list-multiple-keyboard-example';
@@ -13,21 +17,26 @@ export {
     ListOverviewExample,
     ListMultipleCheckboxExample,
     ListMultipleKeyboardExample,
-    ListGroupsExample
+    ListGroupsExample,
+    ListActionButtonExample
 };
 
 const EXAMPLES = [
     ListOverviewExample,
     ListMultipleCheckboxExample,
     ListMultipleKeyboardExample,
-    ListGroupsExample
+    ListGroupsExample,
+    ListActionButtonExample
 ];
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
-        McListModule
+        McListModule,
+        McDropdownModule,
+        McToolTipModule,
+        McIconModule
     ],
     declarations: EXAMPLES,
     exports: EXAMPLES
---
+++ packages/mosaic-examples/mosaic/list/list-action-button/list-action-button-example.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title list ActionButton
+ */
+@Component({
+    selector: 'list-action-button-example',
+    templateUrl: 'list-action-button-example.html',
+    styleUrls: ['list-action-button-example.css']
+})
+export class ListActionButtonExample {
+    selected = [];
+}
