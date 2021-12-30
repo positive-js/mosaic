993a2ce64394e46af5a25358bbcf5358e32c2e93
VSiardziuk <vsiardziuk@ptsecurity.com>
2021-12-30 12:33:03 +0300

    fix(datepicker): fix datepicker for value '31.09.2021' and etc (#UIM-738)

--- packages/mosaic/datepicker/datepicker-input.directive.ts
+++ packages/mosaic/datepicker/datepicker-input.directive.ts
@@ -624,6 +624,13 @@ export class McDatepickerInput<D> implements McFormFieldControl<D>, ControlValue
             date.year, date.month - 1, date.date, date.hours, date.minutes, date.seconds, date.milliseconds
         ));
 
+        if (!newTimeObj) {
+            this.lastValueValid = false;
+            this._value = null;
+
+            return setTimeout(() => this.control.updateValueAndValidity());
+        }
+
         this.lastValueValid = !!newTimeObj;
 
         this.setViewValue(this.getTimeStringFromDate(newTimeObj, this.dateFormats.dateInput), true);
