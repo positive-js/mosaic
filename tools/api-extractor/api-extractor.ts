2bf25e846d4d2aef48ee08301eb0778755d39873
l.s.kramarov <l.s.kramarov@gmail.com>
2022-01-19 11:53:50 +0300

    improved script and added docs

--- tools/api-extractor/api-extractor.ts
+++ tools/api-extractor/api-extractor.ts
@@ -13,17 +13,23 @@ import * as chalk from 'chalk';
 import * as path from 'path';
 
 
-const componentName = process.argv[2];
+const params = process.argv[2];
+
+let localBuild = true;
 
 const buildConfig = JsonFile.load('tools/api-extractor/config.json');
 
-if (componentName) {
-    buildConfig.components = [componentName];
+if (params === 'onlyCheck') {
+    localBuild = false;
+} else if (params) {
+    buildConfig.components = [params];
 }
 
 const configObjectFullPath: string = path.resolve('tools/api-extractor/api-extractor.json');
 const packageJsonLookup: PackageJsonLookup = new PackageJsonLookup();
 const packageJsonFullPath: string | undefined = packageJsonLookup.tryGetPackageJsonFilePathFor(configObjectFullPath);
+
+let hasErrors: boolean = false;
 
 for (const component of buildConfig.components) {
     const configObject: IConfigFile = ExtractorConfig.loadFile(configObjectFullPath);
@@ -41,18 +47,19 @@ for (const component of buildConfig.components) {
     });
 
     const result: ExtractorResult = Extractor.invoke(extractorConfig, {
-        localBuild: true,
+        localBuild,
         showVerboseMessages: true
     });
 
     if (result.succeeded) {
         console.error(chalk.green(`API Extractor completed successfully: ${component}`));
-        process.exitCode = 0;
-    } else {
-        console.error(chalk.red(
-            `API Extractor completed with ${result.errorCount} errors and ${result.warningCount} warnings`
-        ));
-        process.exitCode = 1;
+    } else if (result.errorCount > 0) {
+        console.error(chalk.red(`API Extractor completed with ${result.errorCount} errors`));
+        hasErrors = true;
+    } else if (result.apiReportChanged) {
+        process.exit(1);
     }
 }
+
+process.exitCode = hasErrors ? 1 : 0;
 
