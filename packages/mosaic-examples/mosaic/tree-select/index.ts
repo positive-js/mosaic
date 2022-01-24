3614f53538f20b6b004cc96d29fa84e94d7f1e92
l.s.kramarov <l.s.kramarov@gmail.com>
2022-01-24 16:37:28 +0300

    fix: fixed merge

--- packages/mosaic-examples/mosaic/tree-select/index.ts
+++ packages/mosaic-examples/mosaic/tree-select/index.ts
@@ -5,8 +5,8 @@ import { McHighlightModule } from '@ptsecurity/mosaic/core';
 import { McFormFieldModule } from '@ptsecurity/mosaic/form-field';
 import { McIconModule } from '@ptsecurity/mosaic/icon';
 import { McInputModule } from '@ptsecurity/mosaic/input';
-import { McSelectModule } from '@ptsecurity/mosaic/select';
 import { McProgressSpinnerModule } from '@ptsecurity/mosaic/progress-spinner';
+import { McSelectModule } from '@ptsecurity/mosaic/select';
 import { McTreeModule } from '@ptsecurity/mosaic/tree';
 import { McTreeSelectModule } from '@ptsecurity/mosaic/tree-select';
 
@@ -21,7 +21,7 @@ export {
     TreeSelectOverviewExample,
     TreeSelectMultipleOverviewExample,
     TreeSelectSearchOverviewExample,
-    TreeSelectLazyloadExample
+    TreeSelectLazyloadExample,
     TreeSelectFooterOverviewExample
 };
 
@@ -29,7 +29,7 @@ const EXAMPLES = [
     TreeSelectOverviewExample,
     TreeSelectMultipleOverviewExample,
     TreeSelectSearchOverviewExample,
-    TreeSelectLazyloadExample
+    TreeSelectLazyloadExample,
     TreeSelectFooterOverviewExample
 ];
 
