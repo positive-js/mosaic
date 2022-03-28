4738d9d55b63ea553eaf19e658dcb8c43a8e8ead
l.s.kramarov <l.s.kramarov@gmail.com>
2022-03-28 15:57:05 +0300

    use css grid for dl and added logic for width and other

--- packages/mosaic/dl/dl.component.ts
+++ packages/mosaic/dl/dl.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 
 @Component({
@@ -7,12 +9,39 @@ import { Component, ElementRef, ViewEncapsulation } from '@angular/core';
     styleUrls: ['dl.scss'],
     host: {
         class: 'mc-dl',
-        '[class.mc-dl_vertical]': 'false'
+        '[class.mc-dl_vertical]': 'vertical',
+        '[class.mc-dl_wide]': 'wide',
+        '(window:resize)': 'resizeStream.next()'
     },
     encapsulation: ViewEncapsulation.None
 })
-export class McDlComponent {
-    constructor(public elementRef: ElementRef) {}
+export class McDlComponent implements OnDestroy {
+    @Input() minWidth: number = 400;
+    @Input() wide = false;
+
+    vertical = false;
+
+    readonly resizeStream = new Subject<Event>();
+    private readonly resizeDebounceInterval: number = 100;
+
+    private resizeSubscription: Subscription;
+
+
+    constructor(public elementRef: ElementRef) {
+        this.resizeSubscription = this.resizeStream
+            .pipe(debounceTime(this.resizeDebounceInterval))
+            .subscribe(this.updateState);
+    }
+
+    ngOnDestroy() {
+        this.resizeSubscription.unsubscribe();
+    }
+
+    updateState = () => {
+        const { width } = this.elementRef.nativeElement.getClientRects()[0];
+
+        this.vertical = width <= this.minWidth;
+    }
 }
 
 @Component({
@@ -24,19 +53,14 @@ export class McDlComponent {
     },
     encapsulation: ViewEncapsulation.None
 })
-export class McDtComponent {
-    constructor(public elementRef: ElementRef) {}
-}
+export class McDtComponent {}
 
 @Component({
     selector: 'mc-dd',
     template: '<ng-content></ng-content>',
     host: {
-        class: 'mc-dd',
-        '[class.mc-dd]': 'true'
+        class: 'mc-dd'
     },
     encapsulation: ViewEncapsulation.None
 })
-export class McDdComponent {
-    constructor(public elementRef: ElementRef) {}
-}
+export class McDdComponent {}
